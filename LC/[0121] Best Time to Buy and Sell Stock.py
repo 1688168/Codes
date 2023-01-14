e5cedc08848e6ec43ee004130ad0209f0cc77a71
Y Lee <1688168@gmail.com>
2023-01-14 16:02:07 -0500

    daily 2

--- LC/[0121] Best Time to Buy and Sell Stock.py
+++ LC/[0121] Best Time to Buy and Sell Stock.py
@@ -1,3 +1,24 @@
+
+#####################
+# 20230114
+#####################
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        : carry a min sofar value
+        : profit=max(p[ii]-minSoFar, profit)
+        : minSoFar=min(minSoFar, p[ii])
+        """
+        profit=0
+        minSoFar=prices[0]
+        for p in prices:
+            profit=max(profit, p-minSoFar)
+            minSoFar=min(p, minSoFar)
+
+        return max(0, profit)
+
+
+#####################
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_so_far=prices[0]
--- LC/[0152] Maximum Product Subarray.py
+++ LC/[0152] Maximum Product Subarray.py
@@ -1,24 +1,29 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         """
-        : Kadane's algorithm -> max subarray sum
-        : dp[ii] = max(dp[ii-1]+nums[ii], nums[ii])
-        : x [x x x x ii] x
-        : nii is postive, we want dp[ii-1] postive
-        : nii is negative, we wnat dp[ii-1] min
-        : keep both dp1 for max, dp2 for min
+        : nums - ints
+        : -> subarray with largest product
+        : -> return the product
+        : ----
+        : Kadane's Algorithm
+        : -> max subarray sum: dp[ii]=max(dp[ii-1]+num[ii], num[ii])
+        : -> max subarray product:
+        : dp1=1 #hold max ending @ ii
+        : dp2=1 #hold min ending @ ii
+        : dp1=max(dp1[ii-1]*nums[ii], dp2[ii-1]*nums[ii], nums[ii])
+        : dp2=min(dp2[ii-1]*nums[ii], dp2[ii-1]*nums[ii], nums[ii])
+        : res=max(dp1, res)
         """
+
         dp1=1
         dp2=1
-        N=len(nums)
         res=float('-inf')
+
         for nn in nums:
             tmp1=dp1
             tmp2=dp2
-            dp1=max(tmp1*nn, tmp2*nn, nn)
-            dp2=min(tmp1*nn, tmp2*nn, nn)
+            dp1=max(nn, tmp1*nn, tmp2*nn)
+            dp2=min(nn, tmp1*nn, tmp2*nn)
             res=max(res, dp1)
-
+        
         return res
-
-            
