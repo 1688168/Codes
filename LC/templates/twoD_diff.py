51f1ad3deafecaae8af59d763114af4a912f7d61
Y Lee <1688168@gmail.com>
2023-01-21 11:25:08 -0500

    two d diff template

---
+++ LC/templates/twoD_diff.py
@@ -0,0 +1,29 @@
+class diff2d:
+    def __init__(self, m, n):
+        # declare a 2d  to record all the queries
+        self.m=m
+        self.n=n
+        self.diff=[[0]*(n+1) for _ in range(m+1)]
+
+        # declare a 2d list for calculated output
+        self.out=[[0]*(n+1) for _ in range(m+1)]
+
+    # set method to record query
+    def set(self, x0, y0, x1, y1, val):
+        self.diff[x0][y0] += val   # we have extra row and extra col to avoid idx out of bound
+        self.diff[x0][y1+1] -= val
+        self.diff[x1+1][y0] -= val
+        self.diff[x1+1][y1+1] += val
+
+
+    # calc method to compute from diff into out
+    def calc(self):
+        #self.out[0][0]=self.diff[0][0]
+        for ii in range(self.m):
+            for jj in range(self.n):
+                a=0 if ii==0 else self.out[ii-1][jj]
+                b=0 if jj==0 else self.out[ii][jj-1]
+                c=0 if (ii==0 or jj==0) else self.out[ii-1][jj-1]                   
+                d=self.diff[ii][jj] 
+                self.out[ii][jj]=a+b-c+d
+    
