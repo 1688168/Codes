9b8ccdd5fd4c539102c61b9740d326ed446890a0
Y Lee <1688168@gmail.com>
2023-05-02 15:16:51 -0400

    347 binary search

---
+++ LC/[0347] Top K Frequent Elements - binary search.py
@@ -0,0 +1,49 @@
+"""
+* guess a frequency and see if we have it
+"""
+
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        TopK freq elements
+        whenever we are looking for value -> can we leverage binary search?
+        """
+        val2Freq = Counter(nums) # Time: O(N), Space: O(N)
+
+        def countFreqGE(ff):
+            cnt=0
+            for kk, vv in val2Freq.items():
+                if vv >= ff:
+                    cnt += 1
+               
+            return cnt
+
+
+        ll, rr = 0, len(nums)
+
+        # Time: O(logN)
+        # Search for a freq that has at least K elements with freq >= than it
+        while ll < rr: #we are approaching to the best answer, so we cannot return when equal
+    
+            mm = rr-(rr-ll)//2 #can we ever meet ll=rr to exit?
+            #print("ll: ", ll, " rr: ", rr, " mm: ", mm)
+
+            if countFreqGE(mm) >= k: #if freq is GE K -> mm could be an answer but keep reducing
+                ll=mm 
+                # if we have more element with freq > k, 
+                #    -> we need to increase mm_freq to reduce qualified elements
+                # Since mm could be an answer so we do not increment ll
+            else:
+                rr=mm-1
+        
+
+
+        res = []
+        for kk, vv in val2Freq.items():
+            if vv >= ll:
+                res.append(kk)
+        return res
+
+
+
