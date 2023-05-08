4d87ebdbc9210d8321be9ed9e9df544c459e2b03
Y Lee <44484017+1688168@users.noreply.github.com>
2023-05-08 19:56:31 -0400

    0211

--- LC/[0211] Design Add and Search Words Data Structure - TLE.py
+++ LC/[0211] Design Add and Search Words Data Structure - TLE.py
@@ -1,3 +1,58 @@
+"""
+20230503: T: 88.47
+"""
+class TreeNode:
+    def __init__(self):
+        self.isWord=False
+        self.children={}
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root=TreeNode()
+                
+    def addWord(self, word: str) -> None:
+        itr = self.root
+        for c in word:
+            if c not in itr.children:
+                itr.children[c]=TreeNode()
+            itr=itr.children[c]
+        itr.isWord=True
+        
+
+    def search(self, word: str) -> bool:
+        itr=self.root
+        
+        def dfs(w, itr):
+            if itr is None: return False        
+            if w is None or len(w)==0: return itr.isWord
+
+            if w[0] == ".":
+                for kk, vv in itr.children.items():
+                    res = dfs(w[1:], vv)
+                    if res: return True
+                return False
+            else:
+                if w[0] not in itr.children: return False
+                itr=itr.children[w[0]]
+                return dfs(w[1:], itr)
+
+            return itr.isWord
+
+        return dfs(word, itr)
+        
+
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
+
+
+
+
 
 class TrieNode:
     def __init__(self):
