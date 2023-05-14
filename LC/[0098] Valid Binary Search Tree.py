c30a09a50e90c316f13294019447525ec9f48dfa
Y Lee <44484017+1688168@users.noreply.github.com>
2023-05-14 15:33:59 -0400

    binary search

--- LC/[0098] Valid Binary Search Tree.py
+++ LC/[0098] Valid Binary Search Tree.py
@@ -1,3 +1,25 @@
+###########
+# 20230514
+###########
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def isValid(node, lb, rb):
+            if node is None: return True
+            if node.val <= lb or node.val >= rb: return False
+
+            return isValid(node.left, lb, node.val) and isValid(node.right, node.val, rb)
+
+
+        return isValid(root, float('-inf'), float('inf'))
+
+
+#######################
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
--- LC/[0230] Kth Smallest Element in a BST.py
+++ LC/[0230] Kth Smallest Element in a BST.py
@@ -1,3 +1,40 @@
+###############
+# 20230514
+###############
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        """
+        * inorder traversal BST will produce sorted values
+        """
+        cnt=0
+        kth_val=-1
+        def kth(node):
+            nonlocal cnt, kth_val
+            if node is None: return
+            if cnt > k: return
+            kth(node.left)
+            cnt += 1
+            #print("kth_val: ", kth_val)
+            if cnt == k:
+               
+                kth_val=node.val
+                return
+          
+            kth(node.right)
+
+        #print("calling kth")
+        kth(root)
+        return kth_val
+
+
+################################################
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
