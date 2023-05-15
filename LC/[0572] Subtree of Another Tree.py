20cb3a9a13664dcde615d903e553ff7ccc2b9a61
Y Lee <44484017+1688168@users.noreply.github.com>
2023-05-14 22:57:57 -0400

    514

--- LC/[0572] Subtree of Another Tree.py
+++ LC/[0572] Subtree of Another Tree.py
@@ -1,3 +1,46 @@
+#############
+# 20230514
+#############
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        """
+        : 1. in-order traverse the root tree 
+        : 2. when locating a node with value same as subRoot -> 
+             call helper to check if identical structure             
+        """
+        def isSameStructure(rt, srt):
+            if rt is None and srt is None: return True
+            if rt is None: return False
+            if srt is None: return False
+
+            if rt.val != srt.val: return False
+            return isSameStructure(rt.left, srt.left) and isSameStructure(rt.right, srt.right)
+            
+        
+        def inOrder(root):
+            if root is None: return False
+            if root.val == subRoot.val:
+                if isSameStructure(root, subRoot):
+                    return True
+
+            return inOrder(root.left) or inOrder(root.right)
+
+        return inOrder(root)
+
+
+
+        
+
+
+
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
