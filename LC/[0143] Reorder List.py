c1ec58867e9094f1e565b2419bcf34383f7b74eb
Y Lee <1688168@gmail.com>
2023-05-23 20:58:54 -0400

    143

--- LC/[0143] Reorder List.py
+++ LC/[0143] Reorder List.py
@@ -1,3 +1,56 @@
+############
+# 20230523
+############
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if head is None: return
+        slow, fast = head, head
+
+        """
+        1-2-3-4-5
+        1-2-3-4
+        """
+
+        while fast.next is not None and fast.next.next is not None:
+    
+            slow = slow.next
+            fast=fast.next.next
+
+        # reverse the one after slow
+        # merge the two lists
+        itr=slow.next
+        slow.next=None
+        prev, curr = None, itr
+        while itr is not None:
+            nxt=itr.next
+            itr.next=prev
+            prev=itr
+            itr=nxt 
+
+        head2=prev
+
+
+        itr1=head
+        itr2=head2
+        while itr1.next is not None:
+            nxt=itr1.next
+            itr1.next=itr2
+            itr1=nxt
+            nxt=itr2.next           
+            itr2.next=itr1
+            itr2=nxt
+        
+        itr1.next=itr2
+
+##################################
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
