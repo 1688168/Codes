21576de1331042be1553e2c631e7b6277fe9d1c1
Y Lee <1688168@gmail.com>
2023-05-27 16:21:02 -0400

    interval

--- LC/[0057] Insert Interval.py
+++ LC/[0057] Insert Interval.py
@@ -1,3 +1,27 @@
+############
+# 20230527
+############
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res=[]
+
+        ns, ne = newInterval
+
+        for (cs, ce) in intervals:
+            if ns <= ce and ne >= cs:
+                ns = min(cs, ns)
+                ne = max(ce, ne)
+            elif ne < cs:
+                res.append([ns, ne])
+                ns, ne = cs, ce
+            else:
+                res.append([cs, ce])
+        res.append([ns, ne])
+
+        return res
+
+#################
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         (cs, ce) = newInterval
