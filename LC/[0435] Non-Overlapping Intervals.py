25b29d2a729b907777474c836fc305027e311094
Y Lee <1688168@gmail.com>
2023-05-27 17:00:58 -0400

    435

--- LC/[0435] Non-Overlapping Intervals.py
+++ LC/[0435] Non-Overlapping Intervals.py
@@ -1,3 +1,25 @@
+########
+# 20230527
+########
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+
+        (cs, ce) = intervals[0]
+
+        cnt = 0
+        for ns, ne in intervals[1:]:
+            if ns < ce:
+                cnt += 1              
+                if ne < ce:
+                    cs, ce = ns, ne
+            else:
+                cs, ce = ns, ne
+                        
+        return cnt
+
+
+#####################################
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: (x[0], -x[1]))
