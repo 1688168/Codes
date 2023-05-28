e718879aa9ce55ec180b67248f2c9f072a0dacee
Y Lee <1688168@gmail.com>
2023-05-28 13:05:17 -0400

    graph

--- LC/[0261] Graph Valid Tree.py
+++ LC/[0261] Graph Valid Tree.py
@@ -1,3 +1,34 @@
+##################
+# 20230528
+##################
+
+from collections import defaultdict, deque
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if n == 0: return True
+
+        g=defaultdict(set) 
+
+        for a, b in edges:
+            g[a].add(b)
+            g[b].add(a)
+
+        visited=set()
+        def hasCycle(parent, nn, visited):
+            if nn in visited: return True
+            visited.add(nn)
+
+
+            for child in g[nn]:
+                if child == parent: continue
+                if hasCycle(nn, child, visited): return True            
+
+            return False
+
+        
+        return (not hasCycle(-1, 0, visited)) and len(visited)==n
+
+############################
 from collections import defaultdict
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
