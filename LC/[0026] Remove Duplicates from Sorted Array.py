8692aed7eb06301c64b8ad58d844eb3005f01ce3
Y Lee <1688168@gmail.com>
2023-06-06 00:30:36 -0400

    array

---
+++ LC/[0026] Remove Duplicates from Sorted Array.py
@@ -0,0 +1,36 @@
+##############
+# 20230605
+##############
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        N = len(nums)
+        ii=1
+        """
+        1,2,2
+          i j
+
+        0 1 2 3 4 2 2 3 3 4
+                k
+                           i
+            j  
+        """
+        kk=ii=1
+        while ii < N: # ii indicate the last unique num we identified so far
+            if nums[ii] == nums[kk-1]:
+                jj=ii+1
+                while jj < N and nums[jj]==nums[kk]:
+                    jj+=1
+                if jj < N:
+                    nums[kk]=nums[jj]                        
+                else:
+                    break
+                
+            else:
+                nums[kk]=nums[ii]
+            while ii < N and nums[ii] <= nums[kk]: ii += 1
+            kk += 1
+            
+        
+
+        return kk
