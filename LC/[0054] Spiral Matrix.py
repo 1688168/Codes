7a5b5f85c30357bd28b67d0ff84e425f2faba1b5
Y Lee <1688168@gmail.com>
2023-06-17 16:30:27 -0400

    54

--- LC/[0054] Spiral Matrix.py
+++ LC/[0054] Spiral Matrix.py
@@ -3,80 +3,83 @@
 ###########
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        m=len(matrix)
-        n=len(matrix[0])
+        m = len(matrix)
+        n = len(matrix[0])
 
         ll, rr = 0, n-1
         tt, bb = 0, m-1
-        res=[]
+        res = []
 
-        while ll <= rr and bb >= tt:          
+        while ll <= rr and bb >= tt:
             # top left to right
-            for ii in range(rr-ll+1):              
+            for ii in range(rr-ll+1):
                 res.append(matrix[tt][ll+ii])
             tt += 1
 
-            if tt > bb: break
+            if tt > bb:
+                break
             # top right to bottom
-            for ii in range(bb-tt+1):               
+            for ii in range(bb-tt+1):
                 res.append(matrix[tt+ii][rr])
             rr -= 1
 
-            if rr < ll: break
+            if rr < ll:
+                break
 
             # bottom right to left
             for ii in range(rr-ll+1):
                 res.append(matrix[bb][rr-ii])
             bb -= 1
 
-            if bb< tt: break
+            if bb < tt:
+                break
 
             # bottom left to top
             for ii in range(bb-tt+1):
                 res.append(matrix[bb-ii][ll])
             ll += 1
-            if ll > rr: break
-        
+            if ll > rr:
+                break
+
         return res
-
-
 
 
 ################################
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        M=len(matrix)
-        N=len(matrix[0])
+        M = len(matrix)
+        N = len(matrix[0])
 
-        top=0
-        btm=M
-        left=0
-        right=N
+        top = 0
+        btm = M
+        left = 0
+        right = N
 
-        res=[]
+        res = []
         while top < btm and left < right:
-            #print("top: ", top, " btm: ", btm, " left: ", left, " right: ", right)
-            #output top
+            # print("top: ", top, " btm: ", btm, " left: ", left, " right: ", right)
+            # output top
             for ii in range(left, right):
                 res.append(matrix[top][ii])
 
             top += 1
 
-            #output right
+            # output right
             for ii in range(top, btm):
                 res.append(matrix[ii][right-1])
 
-            right -=1
+            right -= 1
 
-            #check if still valid
-            if top == btm or left==right: break
+            # check if still valid
+            if top == btm or left == right:
+                break
 
-            #output btm
+            # output btm
             for ii in reversed(range(left, right)):
                 res.append(matrix[btm-1][ii])
-            btm -=1
+            btm -= 1
 
-            #output left
+            # output left
             for ii in reversed(range(top, btm)):
                 res.append(matrix[ii][left])
 
--- LC/[0424] Longest Repeating Character Replacement.py
+++ LC/[0424] Longest Repeating Character Replacement.py
@@ -1,3 +1,7 @@
+########
+# 20230521
+# O(N)
+########
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         """
@@ -7,13 +11,40 @@ class Solution:
         ll, rr, N = 0, 0, len(s)
 
         char2cnt = {}
-        mxl=0
+        mxl = 0
+        mxf = 0
         for rr in range(N):
-            c=s[rr]
-            char2cnt[c]=char2cnt.get(c, 0) + 1
-            while rr-ll+1 - max(char2cnt.values()) > k:               
+            c = s[rr]
+            char2cnt[c] = char2cnt.get(c, 0) + 1
+            mxf = max(mxf, char2cnt[c])
+            while rr-ll+1 - mxf > k:
                 char2cnt[s[ll]] -= 1
                 ll += 1
-            mxl=max(mxl, rr-ll+1)
+            mxl = max(mxl, rr-ll+1)
+
+        return mxl
+
+
+########################
+# O(26N)
+########################
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        """
+        * can replace k for longest repeating string
+        * rr-ll+1-max(cnt) <= k
+        """
+        ll, rr, N = 0, 0, len(s)
+
+        char2cnt = {}
+        mxl = 0
+        for rr in range(N):
+            c = s[rr]
+            char2cnt[c] = char2cnt.get(c, 0) + 1
+            while rr-ll+1 - max(char2cnt.values()) > k:
+                char2cnt[s[ll]] -= 1
+                ll += 1
+            mxl = max(mxl, rr-ll+1)
 
         return mxl
