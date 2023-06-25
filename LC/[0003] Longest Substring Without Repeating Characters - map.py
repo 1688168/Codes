b1b1f566c6eb550da3a832d3fa3d30fa022681a2
Y Lee <1688168@gmail.com>
2023-06-25 16:03:53 -0400

    3

--- LC/[0003] Longest Substring Without Repeating Characters - map.py
+++ LC/[0003] Longest Substring Without Repeating Characters - map.py
@@ -1,21 +1,53 @@
+#######
+# 20230625
+#######
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        ll=rr=0
-        char2idx={}
-        N=len(s)
+        """
+        a b c a b c b b
+                      r
+                  l 
 
-        mxl=0
-        for rr in range(N):
-            rchar=s[rr]
-            if rchar in char2idx:
-                ll=max(ll, char2idx[s[rr]]+1)
+        p w w k e w
+                r
+            l   
+        """
 
-            char2idx[rchar]=rr
+        N = len(s)
+        ll = 0
+        char2idx = {}
+        mxl = min(N, 1)
+        for rr, cc in enumerate(s):
+            if cc not in char2idx or char2idx[cc] < ll:
+                mxl = max(mxl, rr-ll+1)
+                char2idx[cc] = rr
 
-            mxl=max(mxl, rr-ll+1)
-
+            else:
+                ll = max(ll, char2idx[cc]+1)
+                char2idx[cc] = rr
+            # print("ll: ", ll, " rr: ", rr, " mxl: ", mxl)
 
         return mxl
 
 
-            
+#############
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ll = rr = 0
+        char2idx = {}
+        N = len(s)
+
+        mxl = 0
+        for rr in range(N):
+            rchar = s[rr]
+            if rchar in char2idx:
+                ll = max(ll, char2idx[s[rr]]+1)
+
+            char2idx[rchar] = rr
+
+            mxl = max(mxl, rr-ll+1)
+
+        return mxl
