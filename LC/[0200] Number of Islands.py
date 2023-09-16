b883d688b1d0192e34a5fe74186eca66a8800684
Y Lee <44484017+1688168@users.noreply.github.com>
2023-09-16 09:53:49 -0400

    200

--- LC/[0200] Number of Islands.py
+++ LC/[0200] Number of Islands.py
@@ -1,35 +1,77 @@
+##########
+# 20230916
+##########
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        dirs=[(1,0), (-1,0), (0, 1), (0, -1)]
+        """
+        - traverse all grid: Time: O(M*N)
+        - for each '1' encountered: dfs and mark visited
+        """
+        M = len(grid)
+        N = len(grid[0])
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        M=len(grid)
-        if M==0: return 0
-        N=len(grid[0])
+        visited = set()
+        count = 0
+
+        def dfs(ii, jj):
+            if ii < 0 or ii >= M or jj < 0 or jj >= N:
+                return
+            if (ii, jj) in visited:
+                return
+            visited.add((ii, jj))
+            curr = grid[ii][jj]
+            if curr == '0':
+                return
+            for dx, dy in dirs:
+                dfs(ii+dx, jj+dy)
+
+        for ii in range(M):
+            for jj in range(N):
+                curr = grid[ii][jj]
+                if curr == '1' and (ii, jj) not in visited:
+                    count += 1
+                    dfs(ii, jj)
+
+        return count
+
+##################################
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        M = len(grid)
+        if M == 0:
+            return 0
+        N = len(grid[0])
 
         def isValid(x, y):
             if x < 0 or y < 0 or x >= M or y >= N:
-                return False # out of bound
-            if grid[x][y] == '2' or grid[x][y]=='0': return False #visited or water
+                return False  # out of bound
+            if grid[x][y] == '2' or grid[x][y] == '0':
+                return False  # visited or water
 
             return True
 
-        cnt=0
+        cnt = 0
 
         def dfs(xx, yy):
             if not isValid(xx, yy):
                 return
 
-            grid[xx][yy]='2'
+            grid[xx][yy] = '2'
 
             for dx, dy in dirs:
-                nx, ny=xx+dx, yy+dy
-                if not isValid(nx, ny): continue
+                nx, ny = xx+dx, yy+dy
+                if not isValid(nx, ny):
+                    continue
                 dfs(nx, ny)
-
 
         for ii in range(M):
             for jj in range(N):
-                if grid[ii][jj]=='1':
+                if grid[ii][jj] == '1':
                     cnt += 1
                     dfs(ii, jj)
 
--- README.md
+++ README.md
@@ -125,7 +125,7 @@
 [`Binary Search`]
 
 [[2638] - <span style="color:red">H</span> - Count the Number of K-Free Subsets](https://leetcode.com/problems/count-the-number-of-k-free-subsets/description) - 
-[[Python]]()
+[[Python]](https://github.com/1688168/Leetcode/blob/main/LC/%5B2638%5D%20Count-the-Number-of-K-Free-Subsets.py)
 [[CPP]](https://github.com/wisdompeak/LeetCode/tree/master/Dynamic_Programming/2638.Count-the-Number-of-K-Free-Subsets) -
 [[Video]](https://www.youtube.com/watch?v=qw0OVFVg5ng) -
 [`Patterns`]
