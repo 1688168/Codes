321dcda4bece046401781877d0718e6ef95846e2
Y Lee <44484017+1688168@users.noreply.github.com>
2023-09-16 21:34:05 -0400

    Update [0937] Reorder Data in Log Files.py

--- LC/[0937] Reorder Data in Log Files.py
+++ LC/[0937] Reorder Data in Log Files.py
@@ -6,3 +6,27 @@ class Solution:
             return (0, rest, _id) if rest[0].isalpha() else (1, )
 
         return sorted(logs, key=get_key)
+
+
+###########
+# 20230916
+###########
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        digit_row = []
+        letter_row = []
+
+        for row in logs:
+            lrow = row.split(" ")
+            if lrow[1].isnumeric():
+                digit_row.append(row)
+            else:
+                letter_row.append(row)
+
+        def my_sort(row):
+            rowl = row.split(" ")
+            return (" ".join(rowl[1:]), rowl[0])
+        letter_row.sort(key=my_sort)
+
+        return letter_row+digit_row
