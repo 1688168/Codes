dac91d53b8795a104393cb9584e73e25e8c1c466
Y Lee <44484017+1688168@users.noreply.github.com>
2023-09-26 08:00:51 -0400

    Update [1151] Minimum Swaps to Group Al 1s Together.py

--- LC/[1151] Minimum Swaps to Group Al 1s Together.py
+++ LC/[1151] Minimum Swaps to Group Al 1s Together.py
@@ -1,3 +1,31 @@
+#############
+# 20230926
+#############
+class Solution:
+    def minSwaps(self, data: List[int]) -> int:
+        window_size = sum(data)
+        N = len(data)
+        if window_size == 0 or window_size == N:
+            return 0
+        min_swap = num_swap = window_size-sum(data[:window_size])
+
+        for ii in range(window_size, N):
+            if data[ii] == 0:
+                num_swap += 1
+
+            """
+            0 1 2 3
+            0 1 0 1
+            ^   ^ 
+              ^   ^
+            """
+            if data[ii-window_size] == 0:
+                num_swap -= 1
+            min_swap = min(min_swap, num_swap)
+        return min_swap
+
+
+################################
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
         num_of_ones = sum(data)  # Time: O(N)
