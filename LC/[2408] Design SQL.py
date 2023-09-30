bdb59da5fa9983e05aeca95d5f5ebd646216dd44
Y Lee <44484017+1688168@users.noreply.github.com>
2023-09-30 18:53:08 -0400

    2408

--- LC/[2408] Design SQL.py
+++ LC/[2408] Design SQL.py
@@ -1,3 +1,27 @@
+###################
+# 20230930
+###################
+from collections import defaultdict
+
+
+class SQL:
+
+    def __init__(self, names: List[str], columns: List[int]):
+        self.tbl2row = defaultdict(list)
+
+    def insertRow(self, name: str, row: List[str]) -> None:
+        self.tbl2row[name].append(row)
+
+    def deleteRow(self, name: str, rowId: int) -> None:
+        self.tbl2row[name][rowId-1] = None
+
+    def selectCell(self, name: str, rowId: int, columnId: int) -> str:
+        return self.tbl2row[name][rowId-1][columnId-1]
+
+################################
+################################
+
+
 class SQL:
 
     def __init__(self, names: List[str], columns: List[int]):
