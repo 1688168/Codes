b862054716215ec506265cf94c147d540e7c3ec4
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-05 20:57:03 -0400

    Update [2307] Check For Contraditions In Equations.py

--- LC/[2307] Check For Contraditions In Equations.py
+++ LC/[2307] Check For Contraditions In Equations.py
@@ -1,4 +1,55 @@
+##########
+# 20231005
+##########
 from collections import defaultdict
+
+
+class Solution:
+    def checkContradictions(self, equations: List[List[str]], values: List[float]) -> bool:
+        """
+        - build a weighted graph
+        - start from each node with starting value 1 to check if any contradiction
+        """
+        g = defaultdict(set)
+
+        def is_equal(a, b):
+            return abs(a-b) < 1e-5
+
+        for (a, b), v in zip(equations, values):
+            if a == b and not is_equal(v, 1):
+                return True
+            g[a].add((b, v))
+            g[b].add((a, 1/v))
+
+        checked = {}
+
+        def has_contradiction(a):
+            """
+            a/b=v 
+            b=a/v
+            """
+
+            for b, v in g[a]:
+                if b in checked:
+                    if not is_equal(checked[b], checked[a]/v):
+                        return True
+                else:
+                    checked[b] = checked[a]/v
+                    if has_contradiction(b):
+                        return True
+
+            return False
+
+        for a in g:
+            if a not in checked:
+                checked[a] = 1
+            if has_contradiction(a):
+                return True
+
+        return False
+
+
+########################################
 
 
 class Solution:
