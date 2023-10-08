7189168e212376713153c0d64342e10d66a2efd1
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-08 14:27:21 -0400

    Update [2222] Number of Ways to Select Buildings - TLE.py

--- LC/[2222] Number of Ways to Select Buildings - TLE.py
+++ LC/[2222] Number of Ways to Select Buildings - TLE.py
@@ -1,3 +1,65 @@
+######################
+# 20231008
+######################
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+
+        s = '#'+s
+        N = len(s)
+
+        dp = [[[0]*2 for _ in range(4)] for _ in range(N)]
+
+        dp[0][0][0] = 1
+        dp[0][0][1] = 1
+
+        for ii in range(1, N):
+            for jj in range(4):
+                for kk in range(2):
+                    if jj == 0:
+                        dp[ii][jj][kk] = dp[ii-1][jj][kk]
+                    else:
+                        dp[ii][jj][kk] = dp[ii-1][jj][kk]
+                        if int(s[ii]) == kk:
+                            dp[ii][jj][kk] += dp[ii-1][jj-1][1-int(s[ii])]
+
+        return dp[-1][-1][0]+dp[-1][-1][1]
+
+
+###############################################
+
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        """
+        Number of ways: -> number of ways to climb stair
+        at each index II -> make decision
+        we can derive current state via finished sub-problems -> DP
+        dp[ii][jj][kk]:  number of valid ways @ ii to select jjth buildings which is type kk 
+        """
+        s = '#' + s
+        N = len(s)
+        building_num = 3
+        dp = [[[0]*2 for _ in range(building_num+1)] for _ in range(N)]
+
+        # define initial state
+        dp[0][0][0] = 1
+        dp[0][0][1] = 1
+        for ii in range(1, N):  # for each building select or not select
+            for jj in range(building_num+1):  # select 0, 1, 2, 3 buildings
+                for kk in range(2):  # building type 0 or 1
+                    if jj == 0:
+                        dp[ii][jj][kk] = dp[ii-1][jj][kk]
+                    else:  # jj >= 1
+                        # not select
+                        dp[ii][jj][kk] = dp[ii-1][jj][kk]
+
+                        # select
+                        if ord(s[ii]) - ord('0') == kk:
+                            dp[ii][jj][kk] += dp[ii-1][jj-1][1-kk]
+
+        return dp[-1][-1][1] + dp[-1][-1][0]
+
+
+###############################################
 class Solution:
     def numberOfWays(self, s: str) -> int:
         """
