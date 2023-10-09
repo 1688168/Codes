3c345b25c363f255b6e3cfa935a57af9cb41e455
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-09 00:35:22 -0400

    Update [0256] Paint House.py

--- LC/[0256] Paint House.py
+++ LC/[0256] Paint House.py
@@ -26,9 +26,26 @@ class Solution:
 
         return min(red, blue, green)
 
+############################################
+
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        n = len(costs)
+        houses = [[0]*3 for _ in range(n)]
+        houses[0] = costs[0]
+        for i in range(1, n):
+            houses[i][0] = costs[i][0] + min(houses[i-1][1], houses[i-1][2])
+            houses[i][1] = costs[i][1] + min(houses[i-1][0], houses[i-1][2])
+            houses[i][2] = costs[i][2] + min(houses[i-1][1], houses[i-1][0])
+
+        # print(houses) # see how the costs are progagated
+
+        return min(houses[-1])
+
 
 ##################
-# 20231008: this is NOT working
+# 20231008: this is NOT working. How do you know Greedy is not an good option?
 ##################
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
