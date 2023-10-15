acda3711766123e96535c319e92a61b4274a30f5
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-15 14:48:55 -0400

    843

---
+++ LC/[0843] Guess the Word.md
@@ -0,0 +1,19 @@
+### Process of elimination
+
+0: filter out all with any match  
+1: filter out any less than 1 match  
+2: ..  
+3: ..  
+4: ..  
+5: ..  
+6: found
+
+### how to select candidate
+
+- random -> no significant advantage
+- pick the one with highest score -> with chars having max freq sum -> to filter out most words
+
+### Complexity
+
+- Time:
+- Space:
--- LC/[0843] Guess the Word.py
+++ LC/[0843] Guess the Word.py
@@ -1,3 +1,62 @@
+# """
+# This is Master's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+# class Master:
+#     def guess(self, word: str) -> int:
+
+class Solution:
+    def findSecretWord(self, words: List[str], master: 'Master') -> None:
+        candidates = words[:]
+
+        def update_candidates(matches, w):
+            nonlocal candidates
+            new_candidates = []
+
+            for c in candidates:
+                cnt = sum(a == b for a, b in zip(c, w))
+                if matches == 0:
+                    if cnt == 0:
+                        new_candidates.append(c)
+                else:
+                    if cnt == matches:
+                        new_candidates.append(c)
+
+            candidates = new_candidates[:]
+
+        def most_overlapped_word():
+            score_board = [[0]*26 for _ in range(6)]
+
+            # populate score_board for each char
+            for w in candidates:
+                for ii, cc in enumerate(w):
+                    score_board[ii][ord(cc)-ord('a')] += 1
+
+            # find the word with highest score to guess
+            best_score = 0
+            best_word = ""
+            for w in candidates:
+                score = 0
+                for ii, cc in enumerate(w):
+                    score += score_board[ii][ord(cc)-ord('a')]
+                if score > best_score:
+                    best_score = score
+                    best_word = w
+            return best_word
+
+        while candidates:
+
+            best_word = most_overlapped_word()
+            matches = master.guess(best_word)
+
+            if matches == 6:
+                return
+            # update candidates per filter
+            update_candidates(matches, best_word)
+
+
+#########################################################
+#########################################################
 class Solution(object):
     def findSecretWord(self, wordlist, master):
 
