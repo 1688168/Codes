ae0b62686678b5f9823685ddc4d400e938448a98
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-15 16:13:40 -0400

    2281

--- LC/[2281] Sum-of-Total-Strength-of-Wizards.md
+++ LC/[2281] Sum-of-Total-Strength-of-Wizards.md
@@ -29,7 +29,7 @@ ret[ii] = (left*(b-ii) + mid*(x*y) + right*(ii-a))\* nums[ii]
 
 left = nums[a+1]*1 + nums[a+2]*2 + nums[a+3]*3 + nums[a+4]*4
 mid = nums[ii]* x*y
-right = nums[ii+1] * 3 + nums[ii+2]*2 + nums[ii+3] \* 3
+right = nums[ii+1] * 3 + nums[ii+2]*2 + nums[ii+3] \* 1
 
 let presum2 = nums[1] * 1 + nums[2]*2 + ... + nums[n]\*N
 
@@ -42,3 +42,62 @@ presum2[b]-presum2[ii] = nums[ii+1]_(ii+1)+nums[ii+2]_(ii+2)+nums[ii+3]*(ii+3)
 = nums[ii+1]*b+nums[ii+2]*b + nums[ii+3]*b - right
 
 ## => right = presum[ii+1: b]\*b - (presum2[b-1]-presum2[ii])
+
+###############
+
+# 20231015
+
+###############
+"""
+total_strength = weakest_wizard_strength \* sum(strength_in_subarray)
+
+      strength > 10^5 => nlog(n) or O(N)
+      all_subarray -> O(N^2) => X
+
+      for each ii as weakest wizard. calc all subarrays's ttl strength with strength[ii] as weakest
+      0 1 2 3 4 5 6 7 8
+      x x x x x x x x x
+              i
+      ^
+      a               b  #(a,b)=(prev_saller_equal, next_smaller)
+
+      x = ii-a
+      y = b-ii
+
+      subarray_strength = left + mid + right
+
+      left = (nums[a+1] * 1 + nums[a+2] * 2 + nums[a+3]*3 + nums[a+4] * 4)*nums[ii]*y
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+              what we want left
+
+      mid = x*y* nums[ii]
+      right = (nums[b-1] * 1 + nums[b-2]*2 + nums[b-3]*3) * nums[ii]*x
+               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+               what we want right
+
+      let presum2 = nums[1]*1 + nums[2]*2 + ... + nums[k]*k
+      presum2[ii]-presum2[a]
+          = nums[a+1]*(a+1) + nums[a+2]* (a+2)+ nums[a+3]*(a+3) + nums[a+4]*(a+4)
+          = "what we want left" + presum[a+1:ii]*a
+      "what we want left" = presum2[ii]-presum2[a]- presum[a+1:ii]*a
+
+      presum2[b]-presum2[ii] =
+        nums[ii+1]*(b-ii-1)+nums[ii+2]*(b-ii-2) + nums[ii+3]*(b-ii-3)
+      = nums[b-3]*(b-ii-1)+nums[b-2]*(b-ii-2)+nums[b-1]*(b-ii-3)
+      = nums[b-1]*(b-ii-3)+nums[b-2]*(b-ii-2)+nums[b-3]*(b-ii-1)
+
+      = nums[b-1]*b + nums[b-2]*b+nums[b-3]*b - "what we want right"
+      = presum[ii+1:b]*b - "what we want right"
+
+      => "what we want right" = presum[ii+1"b]*b-presum2[b]+presum2[ii]
+
+      ttl_strength =
+        "what we want left"*nums[ii]*y
+      + mid*x*y
+      + "what we want right"*nums[ii]*x
+
+      + prepare prev/next smaller to identify range of weakest strength
+      + prepare presum
+      + prepare presum2
+
+      """
--- LC/[2281] Sum-of-Total-Strength-of-Wizards.py
+++ LC/[2281] Sum-of-Total-Strength-of-Wizards.py
@@ -1,52 +1,26 @@
-        """
-        => sum(min*sub_sum)
-        a x x x x ii x x x b
-          1 2 3 4    3 2 1      
-        
-        ttl = (left * (b-ii) + nums[ii] * (b-ii) * (ii-a) + right*(ii-a)) * nums[ii]
-        left = nums[a+1]*1 + nums[a+2]*2 + nums[a+3]*3 + nums[a+4]*4
-        let presum2 = nums[1]*1 + nums[2]*2 + ...
 
-        presum2[ii]-presum2[a] = nums[a+1]*(a+1)+nums[a+2]*(a+2) + ...
-                               = presum[a+1:ii] * a + left
-        >>> left = presum2[ii]-presum2[a] - presum[a+1:ii]*a
-
-        -----
-        right = nums[ii+1] * 3 + nums[ii+2]*2 + nums[ii+3]*1
-
-        presum2[b-1]-presum2[ii] = nums[ii+1] * 1 + nums[ii+2] * 2 + ...
-                                 = presum[ii+1: b-1] * b - right        
-
-
-        >>> right = presum[ii+1:b-1] * b - (presum2[b-1] - presum2[ii])
-
-        nums[ii+1] * b - nums[ii+1] * 3
-
-        -> b = ii+ 3 + 1
-        -> b-3 = ii+1
-        """
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def totalStrength(self, strength: List[int]) -> int:
         # M = pow(10, 9)+7
         M = int(1e9 + 7)
-        N = len(strength)  # the original array size
-
         nums = [0] + strength
-        presum1 = [0] * (N + 2)
-        presum2 = [0] * (N + 2)
+        N = len(nums)  # the original array size
+
+        presum1 = [0] * N
+        presum2 = [0] * N
 
         # prepare normal/special range sum array
-        for ii in range(1, N + 1):
+        for ii in range(1, N):
             presum1[ii] = (presum1[ii - 1] + nums[ii]) % M
             presum2[ii] = (presum2[ii - 1] + nums[ii] * ii) % M
 
         # prev/next smaller
         stack = []
-        next_smaller = [N + 1] * (N + 2)
-        prev_smaller = [0] * (N + 2)
-        for ii in range(1, N + 1):
+        next_smaller = [N] * (N)
+        prev_smaller = [0] * (N)
+        for ii in range(1, N):
             while len(stack) > 0 and nums[ii] < nums[stack[-1]]:
                 next_smaller[stack[-1]] = ii
                 stack.pop()
@@ -61,17 +35,18 @@ class Solution:
         # print("presum2: ", presum2)
         # print("presmaller:", prev_smaller)
         # print("nextsmaller: ", next_smaller)
-        for ii in range(1, N + 1):
+        for ii in range(1, N):
             a, b = prev_smaller[ii], next_smaller[ii]
             x = ii - a
             y = b - ii
-            left = (presum2[ii - 1] - presum2[a]) - ((presum1[ii - 1] - presum1[a]) * a % M)
+            left = (presum2[ii - 1] - presum2[a]) - \
+                ((presum1[ii - 1] - presum1[a]) * a % M)
             left = left * y % M
-            right = ((presum1[b - 1] - presum1[ii]) * b % M - (presum2[b - 1] - presum2[ii]))
+            right = ((presum1[b - 1] - presum1[ii]) * b %
+                     M - (presum2[b - 1] - presum2[ii]))
             right = right * x % M
             mm = nums[ii] * x * y % M
 
             res = (res + (left + mm + right) * nums[ii]) % M
 
         return res
-
