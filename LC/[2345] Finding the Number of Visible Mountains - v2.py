838585210f5e26a6875dd96528543d4fa1a59ff4
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-19 22:07:54 -0400

    Update [2345] Finding the Number of Visible Mountains - v2.py

--- LC/[2345] Finding the Number of Visible Mountains - v2.py
+++ LC/[2345] Finding the Number of Visible Mountains - v2.py
@@ -1,3 +1,42 @@
+###############
+# 20231019
+###############
+
+class Solution:
+    def visibleMountains(self, peaks: List[List[int]]) -> int:
+        """
+        1. convert to intervals
+        2. sort
+        3. evaluate who is the dominate mountain
+        """
+        intervals = [(x-y, x+y) for x, y in peaks]  # convert to intervals
+        intervals.sort()  # interval problems typically you need to sort
+        cs, ce = -math.inf, -math.inf
+        is_duplicate = False
+        cnt = 0
+        for ns, ne in intervals:
+            if ns == cs:
+                if ne > ce:
+                    is_duplicate = False
+                    cs, ce = ns, ne
+                    cnt += 1
+
+                if ce == ne:
+                    if not is_duplicate:
+                        cnt -= 1
+                    is_duplicate = True
+            else:
+                if ne > ce:
+                    is_duplicate = False
+                    cs, ce = ns, ne
+                    cnt += 1
+
+        return cnt
+
+############################################
+############################################
+
+
 class Solution:
     def visibleMountains(self, peaks: List[List[int]]) -> int:
         """
@@ -13,7 +52,7 @@ class Solution:
         intervals = [(x-y, x+y) for x, y in peaks]
         intervals.sort()  # intervals always need to sort
         interval2cnt = collections.Counter(intervals)
-        print(" cnts: ", interval2cnt)
+        # print(" cnts: ", interval2cnt)
         N = len(intervals)
         if N == 1:
             return 1
