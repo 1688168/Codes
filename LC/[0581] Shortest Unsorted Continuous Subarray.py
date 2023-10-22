8582c9c0139a82af47bf0dea573a1fec57628e60
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-22 17:11:48 -0400

    Update [0581] Shortest Unsorted Continuous Subarray.py

---
+++ LC/[0581] Shortest Unsorted Continuous Subarray.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        N = len(nums)
+        prev_greater = [-1]*N
+        next_smaller = [N]*N
+
+        stk = []
+        for ii, vv in enumerate(nums):
+            while stk and nums[stk[-1]] > vv:
+                next_smaller[stk[-1]] = ii
+                stk.pop()
+
+            stk.append(ii)
+        stk = []
+        for ii in reversed(range(N)):
+            while stk and nums[stk[-1]] < nums[ii]:
+                prev_greater[stk[-1]] = ii
+                stk.pop()
+            stk.append(ii)
+
+        ll, rr = 0, N-1
+        while ll < N and next_smaller[ll] == N:
+            ll += 1
+        while rr >= 0 and prev_greater[rr] == -1:
+            rr -= 1
+
+        return 0 if rr <= ll else rr-ll+1
