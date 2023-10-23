44a3ce39f707e3f9c8719ca1e857953ab38e9b72
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-23 00:52:13 -0400

    Update [2371] Minimize Maximum Value in a Grid.py

--- LC/[2371] Minimize Maximum Value in a Grid.py
+++ LC/[2371] Minimize Maximum Value in a Grid.py
@@ -1,3 +1,10 @@
+"""
+1. start to update from the min cell (that's why we sort for all cells)
+2. on each update, we update the max of min_rows, min_cols
+3. update min_rows, min_cols be grid[r][c]+1
+"""
+
+
 class Solution:
     def minScore(self, grid: List[List[int]]) -> List[List[int]]:
         # 1 prep
@@ -5,15 +12,17 @@ class Solution:
         row_mins, col_mins = [1]*m, [1]*n
 
         # 2 sorting
+        # so we will start to ulpdate from the smallest cell
         aux = [(grid[i][j], i, j) for i in range(m) for j in range(n)]
         aux.sort()
 
         # 3. resetting
         for i in range(m*n):
             _, r, c = aux[i]
+            # after assigning the min valid value for this cell
             grid[r][c] = max(row_mins[r], col_mins[c])
-            row_mins[r] = grid[r][c] + 1
-            col_mins[c] = grid[r][c] + 1
+            row_mins[r] = grid[r][c] + 1  # the min valid row value is updated
+            col_mins[c] = grid[r][c] + 1  # the min valid col value is updated
 
         # 4 return the res
         return grid
