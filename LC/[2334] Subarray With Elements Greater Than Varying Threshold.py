d1aadb3ecc9dc7c739d34b5aff082fd535346a4f
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-28 17:07:21 -0400

    Update [2334] Subarray With Elements Greater Than Varying Threshold.py

--- LC/[2334] Subarray With Elements Greater Than Varying Threshold.py
+++ LC/[2334] Subarray With Elements Greater Than Varying Threshold.py
@@ -1,3 +1,43 @@
+###############
+# 20231028
+###############
+class Solution:
+    def validSubarraySize(self, nums: List[int], threshold: int) -> int:
+        """
+        1 3 4 3 1
+            x       
+          x x x
+          x x x
+        x x x x x
+
+        min(elements)*k > threshold
+
+        - same as question: 84
+        """
+
+        N = len(nums)
+        nums = [0]+nums+[0]
+        stk = []
+
+        # for each stk[-1] as the min of the subarray
+        for ii, nn in enumerate(nums):
+            while stk and nn < nums[stk[-1]]:  # nn is the smaller
+                curr_ii = stk.pop()
+                curr_h = nums[curr_ii]
+                next_h = nn
+                if stk:  # after first pop, stk[-1] is the previous smaller or equal
+                    prev_h = nums[stk[-1]]
+                    mnh = curr_h*(ii-stk[-1]-1)
+                    if mnh > threshold:
+                        return ii-stk[-1]-1
+
+            stk.append(ii)
+
+        return -1
+
+##########################################
+
+
 class Solution:
     def validSubarraySize(self, nums: List[int], threshold: int) -> int:
         """
