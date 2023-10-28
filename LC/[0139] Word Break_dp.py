829008d7306c757e84f8bc1ea355a1d5e85c5ee1
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-28 00:26:37 -0400

    Update [0139] Word Break_dp.py

--- LC/[0139] Word Break_dp.py
+++ LC/[0139] Word Break_dp.py
@@ -1,3 +1,7 @@
+#############
+# 20231028
+#############
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         """
@@ -7,13 +11,37 @@ class Solution:
         dp[ii] = True/False if s[:ii] can be broken into dictionary
                = dp[jj] and s[jj:ii+1] in word_set
         """
-        N=len(s)
-        dp=[False]*(N+1) # length of s 0~N
-        dp[0]=True
-        word_set=set(wordDict)
-        for ii in range(1, N+1): # for each length
+        N = len(s)
+        words = set(wordDict)
+        dp = [False]*(N+1)
+        dp[0] = True  # for empty string, we can choose nothing from dict
+        for ii in range(1, N+1):
             for jj in range(ii):
-                dp[ii]=dp[jj] and (s[jj:ii] in word_set)
-                if dp[ii]: break
+                dp[ii] = (dp[jj] and s[jj:ii] in words)
+                if dp[ii]:
+                    break
+
+        return dp[N]
+
+
+##################################
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """
+        Time: O(N^3): NXN array size, and string comparison in set lookup
+        dp word break
+
+        dp[ii] = True/False if s[:ii] can be broken into dictionary
+               = dp[jj] and s[jj:ii+1] in word_set
+        """
+        N = len(s)
+        dp = [False]*(N+1)  # length of s 0~N
+        dp[0] = True
+        word_set = set(wordDict)
+        for ii in range(1, N+1):  # for each length
+            for jj in range(ii):
+                dp[ii] = dp[jj] and (s[jj:ii] in word_set)
+                if dp[ii]:
+                    break
 
         return dp[N]
