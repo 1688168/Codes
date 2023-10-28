572d532d4b6159c0059d64f4da7978c359b4c2bf
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-28 14:47:20 -0400

    Update [0084] Largest Rectangle in Histogram - one pass.py

--- LC/[0084] Largest Rectangle in Histogram - one pass.py
+++ LC/[0084] Largest Rectangle in Histogram - one pass.py
@@ -1,3 +1,25 @@
+############
+# 20231028
+############
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stk = []
+        heights = [0]+heights+[0]
+        mx = 0
+        for ii, nh in enumerate(heights):
+            while stk and nh < heights[stk[-1]]:
+                # next smaller is ii, prev_smaller or equal is in stk[-1]
+                curr_ii = stk.pop()
+                prev_ii = stk[-1]
+                mx = max(heights[curr_ii]*(ii-prev_ii-1), mx)
+
+            stk.append(ii)
+
+        return mx
+
+##########################
+
+
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         N = len(heights)
