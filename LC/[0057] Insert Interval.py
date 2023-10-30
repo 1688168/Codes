66a28d42b2ba54269b650ba531f37e0a58af1050
Y Lee <44484017+1688168@users.noreply.github.com>
2023-10-30 00:16:08 -0400

    Update [0057] Insert Interval.py

--- LC/[0057] Insert Interval.py
+++ LC/[0057] Insert Interval.py
@@ -1,10 +1,34 @@
 ############
+# 20231030
+############
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        ins, ine = newInterval
+
+        res = []
+        for ns, ne in intervals:
+            # inserting interval is before
+            if ine < ns:
+                res.append((ins, ine))
+                ins, ine = ns, ne
+            elif ine >= ns and ins <= ne:  # has overlap
+                ins = min(ns, ins)
+                ine = max(ne, ine)
+            else:
+                res.append((ns, ne))
+
+        res.append((ins, ine))
+
+        return res
+
+
+############
 # 20230527
 ############
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res=[]
+        res = []
 
         ns, ne = newInterval
 
@@ -22,17 +46,19 @@ class Solution:
         return res
 
 #################
+
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         (cs, ce) = newInterval
 
-        res=[]
+        res = []
         for ns, ne in intervals:
-            if ne <cs:
+            if ne < cs:
                 res.append([ns, ne])
             elif ne >= cs and ns <= ce:
-                cs=min(cs, ns)
-                ce=max(ce, ne)
+                cs = min(cs, ns)
+                ce = max(ce, ne)
             elif ns > ce:
                 res.append([cs, ce])
                 cs, ce = ns, ne
