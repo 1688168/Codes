0c0201046e2f1624df0036011c8e2b57e2642c45
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-04 17:14:29 -0400

    Update [2313] Minimum Flips in Binary Tree to Get Result.py

--- LC/[2313] Minimum Flips in Binary Tree to Get Result.py
+++ LC/[2313] Minimum Flips in Binary Tree to Get Result.py
@@ -1,9 +1,73 @@
+###########
+# 20231104
+###########
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class Solution:
+    """
+    memo is the best friend of dfs
+    """
+
+    def minimumFlips(self, root: Optional[TreeNode], result: bool) -> int:
+
+        memo = {}
+
+        # @cache
+        def dfs(root, res):
+            if root.left is None and root.right is None:  # a leaf node
+                return int((int(res) != root.val))
+
+            if root is None:  # sanity check
+                return 0
+
+            if (root, res) in memo:
+                return memo[(root, res)]
+
+            if root.val == 2:  # or
+                if res:  # true
+                    ans = min(dfs(root.left, True), dfs(root.right, True))
+                else:
+                    ans = dfs(root.left, False)+dfs(root.right, False)
+            elif root.val == 3:  # and
+                if res:
+                    ans = dfs(root.left, True)+dfs(root.right, True)
+                else:
+                    ans = min(dfs(root.left, False), dfs(root.right, False))
+
+            elif root.val == 4:  # xor
+                if res:
+                    a1 = dfs(root.left, True) + dfs(root.right, False)
+                    a2 = dfs(root.left, False) + dfs(root.right, True)
+                    ans = min(a1, a2)
+                else:
+                    a1 = dfs(root.left, True)+dfs(root.right, True)
+                    a2 = dfs(root.left, False)+dfs(root.right, False)
+                    ans = min(a1, a2)
+
+            elif root.val == 5:  # not
+                child = root.left if root.left else root.right
+                ans = dfs(child, not res)
+            else:
+                raise Exception(f"what the fuck? {root.val}")
+
+            memo[(root, res)] = ans
+            return ans
+
+        return dfs(root, result)
+
+####################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
 class Solution:
     def minimumFlips(self, root: Optional[TreeNode], result: bool) -> int:
         memo = collections.defaultdict(dict)  # memo is DFS's best friend
