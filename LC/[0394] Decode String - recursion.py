ebdff326a25721d886fd188e889371d9b0294f6a
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-06 21:39:01 -0500

    394

--- LC/[0394] Decode String - recursion.py
+++ LC/[0394] Decode String - recursion.py
@@ -1,3 +1,38 @@
+##############
+# 20231106
+##############
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        cs = ""
+        ii = 0
+        anum = 0
+        N = len(s)
+        while ii < N:
+            char = s[ii]
+            if char.isdigit():
+                jj = ii
+                while jj < N and s[jj].isdigit():
+                    jj += 1
+                anum = int(s[ii:jj])
+                ii = jj
+                continue
+            elif char == "]":  # pop
+                return cs, ii + 1  # this is the length ii travelled in this stack
+            elif char == "[":
+                substring, jj = self.decodeString(s[ii + 1:])
+                cs += anum * substring
+                anum = 0
+                # ii need to increment 1 to move after "[" + jj the distance travelled in the recursion
+                ii += (jj+1)
+            else:  # a char
+                cs += char
+                ii += 1
+
+        return cs
+
+
+##################################
 class Solution:
     def decodeString(self, s: str) -> str:
         cs = ''
--- LC/[0394] Decode String - stack.py
+++ LC/[0394] Decode String - stack.py
@@ -1,3 +1,41 @@
+##################
+# 20231106
+###################
+class Solution:
+    def decodeString(self, s: str) -> str:
+        ii = 0
+        stk_n = []
+        stk_s = []
+        text = ""
+        N = len(s)
+        while ii < N:
+            c = s[ii]
+            if c.isdigit():
+                n = 0
+                jj = ii
+                while jj < N and s[jj].isdigit():
+                    jj += 1
+                n = int(s[ii:jj])
+                ii = jj
+                # after num always follows "["
+
+                continue
+            elif c == "[":
+                stk_n.append(n)
+                stk_s.append(text)
+                text = ""
+            elif c == "]":
+                n = stk_n.pop()
+                ss = stk_s.pop()
+                text = ss + n*text
+            else:
+                text += c
+            ii += 1
+        return text
+
+###################################
+
+
 class Solution:
     def decodeString(self, s: str) -> str:
         ii = 0
