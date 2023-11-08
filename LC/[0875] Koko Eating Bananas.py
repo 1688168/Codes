25c4248db3979e91fc859d4122535657841d3d56
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-07 23:43:23 -0500

    Update [0875] Koko Eating Bananas.py

--- LC/[0875] Koko Eating Bananas.py
+++ LC/[0875] Koko Eating Bananas.py
@@ -1,3 +1,31 @@
+##################
+# 20231107
+##################
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        ll, rr = 1, sum(piles)
+        ans = -1
+        N = len(piles)
+
+        def can_finish(mm):
+            tt = 0
+            for ii, vv in enumerate(piles):
+                tt += math.ceil(vv/mm)
+
+            return True if tt <= h else False
+
+        while ll <= rr:
+            mm = ll+(rr-ll)//2
+            if can_finish(mm):
+                ans = mm
+                rr = mm-1
+            else:
+                ll = mm+1
+
+        return ans
+
+
+########################
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         ll, rr = 1, max(piles)
