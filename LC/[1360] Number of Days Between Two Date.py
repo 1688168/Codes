615800f19c70b0e5e0e2be4348225c358a4164f2
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-08 20:36:01 -0500

    Update [1360] Number of Days Between Two Date.py

--- LC/[1360] Number of Days Between Two Date.py
+++ LC/[1360] Number of Days Between Two Date.py
@@ -1,3 +1,44 @@
+################
+# 2023108
+################
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        """
+        diff of two days = abs(diff(day1, 1971) - diff(day2, 1971)) 
+        """
+
+        def is_leap_year(year):
+
+            if year % 4 != 0:
+                return False
+            elif year % 100 != 0:
+                return True
+            elif year % 400 != 0:
+                return False
+            else:
+                return True
+
+        def leaps(yy, mm):
+            cnt = 0
+            for yr in range(1971, yy):
+                if is_leap_year(yr):
+                    cnt += 1
+            if is_leap_year(yy) and mm > 2:
+                cnt += 1
+            return cnt
+
+        def get_days(date):
+            yy, mm, dd = map(int, date.split("-"))
+            leap_cnt = leaps(yy, mm)
+            mo = [0, 31, 28, 31, 30, 31, 30, 31, 31,
+                  30, 31, 30, 31]  # this starts from zero
+
+            return leap_cnt + (yy-1971)*365+sum(mo[:mm])+dd
+
+        return abs(get_days(date2)-get_days(date1))
+
+
+################################
 class Solution:
     def daysBetweenDates(self, date1: str, date2: str) -> int:
         r1 = self.get_days(date1)
