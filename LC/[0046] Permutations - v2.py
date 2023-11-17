8bd59912b37836ef90079b2f2ca83310139b2a2b
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-16 23:23:21 -0500

    Update [0046] Permutations - v2.py

--- LC/[0046] Permutations - v2.py
+++ LC/[0046] Permutations - v2.py
@@ -1,19 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        N = len(nums)
-        # if N <=1: return [nums]
         res = []
+        N = len(nums)
 
         def helper(st, nums):
             if st == N:
                 res.append(nums)
                 return
-            # if st > N: return None
 
             for ii in range(st, N):
+                # fix st and swap with all
                 nums[st], nums[ii] = nums[ii], nums[st]
-                helper(st+1, nums[:])
-                # nums[st], nums[ii] = nums[ii], nums[st]
+                helper(st+1, nums[:])  # next will be fixing st+1
+                nums[st], nums[ii] = nums[ii], nums[st]
 
         helper(0, nums)
+
         return res
