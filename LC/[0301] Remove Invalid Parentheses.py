4683f0264e8de701702df2f969c8c7ff365d6774
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-18 14:10:29 -0500

    Update [0301] Remove Invalid Parentheses.py

--- LC/[0301] Remove Invalid Parentheses.py
+++ LC/[0301] Remove Invalid Parentheses.py
@@ -1,3 +1,67 @@
+###########
+# 20231118
+###########
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        """
+        1. determine the max_len (calc the min remove cnt)
+        2. DFS to find all valid parentheses after removal
+        - think of memo
+        - the pruning strategy
+        - remove duplicate strategy
+        """
+        N = len(s)
+        left_cnt = 0
+        remove_cnt = 0
+        for ii, cc in enumerate(s):
+            if cc.isalpha():
+                continue
+            elif cc == "(":
+                left_cnt += 1
+            else:
+                if left_cnt > 0:
+                    left_cnt -= 1
+                else:
+                    remove_cnt += 1
+
+        remove_cnt += left_cnt
+        valid_len = N-remove_cnt
+        print(" expected parentheses len: ", valid_len)
+
+        res = []
+
+        def dfs(st, path, left_cnt):
+            if left_cnt < 0:
+                return
+            if len(path) > valid_len:
+                return
+            # pruning
+            if st == N and len(path) == valid_len and left_cnt == 0:
+                res.append(path)
+                return
+
+            if st >= N:
+                return
+            # remove duplicate
+            """
+            if s[ii] == path[-1]: must select
+            else:
+                select or not select
+            """
+            cc = s[st]
+            if cc.isalpha():
+                dfs(st+1, path+s[st], left_cnt)
+            else:
+                dfs(st+1, path+cc, left_cnt+(1 if cc == "(" else -1))
+                if path == "" or cc != path[-1]:
+                    dfs(st+1, path, left_cnt)
+
+        path = ""
+        dfs(0, path, 0)
+        return res
+
+
+####################################
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
         """
