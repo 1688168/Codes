d1583ab19d96f5e641a8536ca5a580d896d26afd
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-17 23:59:50 -0500

    Update [0921] Minimum Add to Make Parentheses Valid.py

--- LC/[0921] Minimum Add to Make Parentheses Valid.py
+++ LC/[0921] Minimum Add to Make Parentheses Valid.py
@@ -1,3 +1,25 @@
+#############
+# 20231117
+#############
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        N = len(s)
+        left_cnt = 0
+        cnt = 0
+        for ii in range(N):
+            if (cc := s[ii]) == "(":
+                left_cnt += 1
+            elif cc.isalpha():
+                continue
+            else:
+                if left_cnt > 0:
+                    left_cnt -= 1
+                else:
+                    cnt += 1
+        return cnt+left_cnt
+
+
+##########################
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
 
