40540d73273e9804ed5504e5b59679e9fd080a24
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-19 11:02:54 -0500

    Update [0772] Basic Calculator III - var state and recursion.py

--- LC/[0772] Basic Calculator III - var state and recursion.py
+++ LC/[0772] Basic Calculator III - var state and recursion.py
@@ -1,3 +1,71 @@
+######################
+# 20231119
+#######################
+class Solution:
+    def calculate(self, s: str) -> int:
+        """
+        commit (+,-)
+        pending ()
+        """
+        N = len(s)
+
+        def calc(pending, nn, op):
+            if op == "+":
+                return (pending+nn)
+
+            elif op == "-":
+                return (pending-nn)
+
+            elif op == "*":
+                pending *= nn
+                return pending
+            elif op == "/":
+                sign = (1 if pending*nn >= 0 else -1)
+                return abs(pending)//abs(nn)*sign
+            else:
+                msg = f"unexpected op=[{op}]"
+                raise Exception(msg)
+
+        def dfs(ii):
+            op = "+"
+            committed = 0
+            pending = 0
+
+            while ii < N:
+                cc = s[ii]
+                if cc.isdigit():  # parese num
+                    jj = ii
+                    while jj < N and s[jj].isdigit():
+                        jj += 1
+                    nn = int(s[ii:jj])
+                    pending = calc(pending, nn, op)
+                    ii = jj
+                    continue
+                else:
+                    if cc == ' ':
+                        pass
+                    elif cc in ['+', '-', '*', '/']:
+                        op = cc
+                        if op in ["+", "-"]:
+                            committed += pending
+                            pending = 0
+                    elif cc == "(":
+                        ii, nn = dfs(ii+1)
+                        pending = calc(pending, nn, op)
+                        continue
+                    elif cc == ")":
+                        return ii+1, committed+pending
+                    else:
+                        msg = f"unexpected ch=[{c}]"
+                        raise Exception(msg)
+                    ii += 1
+            return ii, committed+pending
+
+        return dfs(0)[1]
+
+##########################################################
+
+
 class Solution:
     def calculate(self, s: str) -> int:
         """
@@ -8,54 +76,55 @@ class Solution:
         * ('*', '/'): handle by varible state
         * parenthesis: leverage recursion
         """
-        ii=0
-        N=len(s)
+        ii = 0
+        N = len(s)
+
         def calculator():
             nonlocal ii
 
-            committed=0
-            pending=0
-            op='+'
+            committed = 0
+            pending = 0
+            op = '+'
 
             def calc(num):
                 nonlocal pending
-                if op=='+':
+                if op == '+':
                     pending += num
-                elif op=='-':
+                elif op == '-':
                     pending -= num
-                elif op=='*':
+                elif op == '*':
                     pending *= num
-                elif op=='/':
-                    sign=1 if pending*num >=0 else -1
+                elif op == '/':
+                    sign = 1 if pending*num >= 0 else -1
                     pending = abs(pending)//abs(num)*sign
                 else:
                     raise Exception(f"Invalid operator {op}")
 
             while ii < N:
-                c=s[ii]
-                if c==' ':
-                    ii+=1
+                c = s[ii]
+                if c == ' ':
+                    ii += 1
                     continue
 
-                if c.isdigit(): # got a number
-                    #pars out the num
-                    jj=ii
-                    while jj< N and s[jj].isdigit():
-                        jj+=1
-                    num=int(s[ii:jj])
+                if c.isdigit():  # got a number
+                    # pars out the num
+                    jj = ii
+                    while jj < N and s[jj].isdigit():
+                        jj += 1
+                    num = int(s[ii:jj])
                     calc(num)
-                    ii=jj
+                    ii = jj
                 else:
-                    ii+=1
+                    ii += 1
                     if c in ('+', '-'):
-                        op=c
+                        op = c
                         committed += pending
-                        pending=0
+                        pending = 0
                     elif c in ('*', '/'):
-                        op=c
+                        op = c
                     elif c == '(':
                         calc(calculator())
-                    elif c==')':
+                    elif c == ')':
                         return committed+pending
                     else:
                         print("what's this?: ", c)
