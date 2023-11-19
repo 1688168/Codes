2a381b6984f8aec45962fc798775da09478a9798
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-19 01:04:35 -0500

    Update [0719] Find K-th Smallest Pair Distance - v0.py

--- LC/[0719] Find K-th Smallest Pair Distance - v0.py
+++ LC/[0719] Find K-th Smallest Pair Distance - v0.py
@@ -1,37 +1,38 @@
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        """
-        1. sort: nlogn
-        2. Binary Search: while loop
-        3. outter loop: N
-        4. bisect_right: logN
-        => 32NlogN
-        """
         nums.sort()
         N = len(nums)
+        ll = min([b-a for a, b in zip(nums[:-1], nums[1:])])
         rr = nums[-1]-nums[0]
-        ll = 0
-        ans = -1
+        ans = rr
 
-        while ll < rr:  # 32
-            mm = (ll+rr) >> 1  # mm is the guessed distance
-            cnt = 0
-
-            # we need to count number of pairs with distance < mm
-            # N -- we fix the first number and binary search the next (NlogN)
+        def count(mm):
+            ii = 0
             jj = 0
+            cnt = 0
+            # while ii<N:
+            #     while jj<N and nums[jj]-nums[ii] <= mm:
+            #         jj+=1
+            #     cnt += (jj-ii-1)
+
+            #     ii+=1
+
+            # return cnt
             for ii, vv in enumerate(nums):
 
                 while jj < N and nums[jj]-vv <= mm:
                     jj += 1
 
                 cnt += jj-ii-1
-            if cnt < k:
+            return cnt
+
+        while ll <= rr:
+            mm = ll+(rr-ll)//2
+
+            if count(mm) < k:
                 ll = mm+1
             else:
                 ans = mm
-                rr = mm
-        return ll
+                rr = mm-1
 
-
-##############################
+        return ans
