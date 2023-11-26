3b72f382a1ad0dfff31c5c0f1ee58e2809105cb7
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-26 16:16:03 -0500

    Update [0297] Serialize and Deserialize Binary Tree - BFS.py

--- LC/[0297] Serialize and Deserialize Binary Tree - BFS.py
+++ LC/[0297] Serialize and Deserialize Binary Tree - BFS.py
@@ -1,3 +1,63 @@
+################
+# 20231126
+################
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        dq = collections.deque([root])
+        res = ""
+        while dq:
+            curr = dq.popleft()
+            if curr is None:
+                res += "#,"
+            else:
+                res += str(curr.val)+","
+            if curr is not None:
+                dq.append(curr.left)
+                dq.append(curr.right)
+
+        return res[:-1]
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        tkn = data.split(",")
+        nodes = [TreeNode(t) if t != "#" else None for t in tkn]
+        ii, jj = 0, 1
+        while ii < len(nodes):
+            if nodes[ii] is not None:
+                nodes[ii].left = nodes[jj]
+                nodes[ii].right = nodes[jj+1]
+                jj += 2
+            ii += 1
+
+        return nodes[0]
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+#########################################
 """
 # BFS serialization:
 1. BFS -> deque
@@ -21,7 +81,8 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-from collections import deque
+
+
 class Codec:
     """
     : serialize: into CSV string in level order (BFS):
@@ -38,12 +99,13 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        if root is None: return "" # corner case
+        if root is None:
+            return ""  # corner case
 
-        dq=deque([root])
-        res=""
+        dq = deque([root])
+        res = ""
         while len(dq) > 0:
-            curr=dq.popleft()
+            curr = dq.popleft()
             if curr is not None:
                 res += (str(curr.val)+',')
                 dq.append(curr.left)
@@ -51,9 +113,7 @@ class Codec:
             else:
                 res += '#,'
 
-
         return res[:-1]  # remove the last unused comma
-
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -61,26 +121,25 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        if len(data)==0: return None
-        arr=data.split(",")
+        if len(data) == 0:
+            return None
+        arr = data.split(",")
         print(" arr: ", arr)
-        ii, jj, N=0, 1, len(arr)
-        nodes=[]
-        for ii in range(len(arr)): #place all nodes into a list
+        ii, jj, N = 0, 1, len(arr)
+        nodes = []
+        for ii in range(len(arr)):  # place all nodes into a list
             nodes.append(TreeNode(int(arr[ii])) if arr[ii] != '#' else None)
 
         ii, jj = 0, 1
-        while jj < len(nodes): # link all nodes
+        while jj < len(nodes):  # link all nodes
             if nodes[ii] is not None:
-                nodes[ii].left=nodes[jj]
-                nodes[ii].right=nodes[jj+1]
+                nodes[ii].left = nodes[jj]
+                nodes[ii].right = nodes[jj+1]
                 jj += 2
 
             ii += 1
 
         return nodes[0]
-
-
 
 
 # Your Codec object will be instantiated and called as such:
