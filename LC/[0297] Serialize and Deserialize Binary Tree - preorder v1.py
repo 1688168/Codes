f65fb09b93c2358957a9597466cf7683a795549b
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-26 16:59:19 -0500

    Update [0297] Serialize and Deserialize Binary Tree - preorder v1.py

--- LC/[0297] Serialize and Deserialize Binary Tree - preorder v1.py
+++ LC/[0297] Serialize and Deserialize Binary Tree - preorder v1.py
@@ -1,3 +1,65 @@
+#####################
+# 20231126
+#####################
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    """
+    : preorder can uniquely re-construct the binary tree
+    """
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        # preorder traversal
+        if root is None:
+            return '#'
+        return str(root.val) + ',' + self.serialize(root.left) + ',' + self.serialize(root.right)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        tkn = data.split(",")
+        nodes = [TreeNode(t) if t != '#' else None for t in tkn]
+
+        def getNum(node):
+            if node is None:
+                return 1  # here we return 1 cuz None also take space in the nodes array
+            return 1+getNum(node.left)+getNum(node.right)
+
+        def dfs(nodes):
+            if nodes[0] is None:
+                return None
+
+            left = dfs(nodes[1:])
+            left_sz = getNum(left)
+            right = dfs(nodes[(left_sz+1):])
+
+            nodes[0].left = left
+            nodes[0].right = right
+
+            return nodes[0]
+
+        return dfs(nodes)
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+###############################
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
