7b8cad3004d2b9eff63dee71e44d4053e0e80c73
Y Lee <44484017+1688168@users.noreply.github.com>
2023-11-26 17:03:25 -0500

    Update [0297] Serialize and Deserialize Binary Tree - preorder v2.py

--- LC/[0297] Serialize and Deserialize Binary Tree - preorder v2.py
+++ LC/[0297] Serialize and Deserialize Binary Tree - preorder v2.py
@@ -1,3 +1,6 @@
+#######################
+# 20231126
+#######################
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -6,6 +9,65 @@
 #         self.right = None
 
 from collections import deque
+
+
+class Codec:
+    """
+    : preorder can uniquely re-construct the binary tree
+    """
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        # preorder traversal
+        if root is None:
+            return '#'
+        return str(root.val) + ',' + self.serialize(root.left) + ',' + self.serialize(root.right)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        tkn = data.split(",")
+        dq = collections.deque(
+            [TreeNode(t) if t != '#' else None for t in tkn])
+
+        def dfs(dq):
+            curr = dq.popleft()
+            if curr is None:
+                return None
+
+            left = dfs(dq)
+            right = dfs(dq)
+
+            curr.left = left
+            curr.right = right
+
+            return curr
+
+        return dfs(dq)
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+
+#######################################
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
 class Codec:
 
     def serialize(self, root):
@@ -16,9 +78,9 @@ class Codec:
         : preorder: left most node is the root
         """
         # preorder traversal
-        if root is None: return '#'
-        return str(root.val)+ ',' + self.serialize(root.left)+ ',' + self.serialize(root.right)
-
+        if root is None:
+            return '#'
+        return str(root.val) + ',' + self.serialize(root.left) + ',' + self.serialize(root.right)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -26,22 +88,23 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        arr=data.split(',')
+        arr = data.split(',')
 
-        dq=deque(arr)
+        dq = deque(arr)
 
         def dfs(dq):
-            if dq is None: return None
+            if dq is None:
+                return None
             curr = dq.popleft()
-            if curr =='#': return None
+            if curr == '#':
+                return None
             node = TreeNode(int(curr))
-            node.left=dfs(dq)
-            node.right=dfs(dq)
+            node.left = dfs(dq)
+            node.right = dfs(dq)
 
             return node
 
         return dfs(dq)
-
 
 
 # Your Codec object will be instantiated and called as such:
