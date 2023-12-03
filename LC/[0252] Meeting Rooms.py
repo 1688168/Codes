c5a61ae90fa7fcc679de5b34a19dbfe11ed0fc5c
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-03 14:30:34 -0500

    Update [0252] Meeting Rooms.py

--- LC/[0252] Meeting Rooms.py
+++ LC/[0252] Meeting Rooms.py
@@ -1,13 +1,35 @@
+###########################
+# 20231203
+###########################
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) == 0:
+            return True
+        intervals.sort()
+        ins, ine = intervals[0]
+
+        for ns, ne in intervals[1:]:
+            if ine <= ns:
+                ins, ine = ns, ne
+            elif ins < ne and ns < ine:
+                return False
+
+        return True
+
+
+###########################
+###########################
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
-        if len(intervals) ==0: return True
+        if len(intervals) == 0:
+            return True
         (cs, ce) = intervals[0]
 
         for ns, ne in intervals[1:]:
-            if ns < ce: return False
+            if ns < ce:
+                return False
             cs, ce = ns, ne
 
-
         return True
-        
