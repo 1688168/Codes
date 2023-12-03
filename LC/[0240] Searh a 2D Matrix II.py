375d92022fa739c5d3fdf30be6c7a3dc6f974f5c
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-02 21:23:26 -0500

    Update [0240] Searh a 2D Matrix II.py

--- LC/[0240] Searh a 2D Matrix II.py
+++ LC/[0240] Searh a 2D Matrix II.py
@@ -13,3 +13,25 @@ class Solution:
                 ii -= 1
 
         return False
+    
+
+    #################################
+    # 20231202
+    #################################
+    class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        M=len(matrix)
+        N=len(matrix[0])
+        ii=M-1
+        jj=0
+        while ii >=0 and jj < N:
+     
+            while ii >= 0 and matrix[ii][jj] > target:
+                ii-=1
+            
+            while jj< N and matrix[ii][jj] < target:
+                jj+=1
+            
+            if ii >= 0 and jj < N and matrix[ii][jj]==target: return True
+        
+        return False
