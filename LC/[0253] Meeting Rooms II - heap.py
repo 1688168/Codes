209e31586df555b85c222f7276f0c1c0f633e42f
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-03 14:57:14 -0500

    Update [0253] Meeting Rooms II - heap.py

--- LC/[0253] Meeting Rooms II - heap.py
+++ LC/[0253] Meeting Rooms II - heap.py
@@ -1,7 +1,27 @@
 #########################
-# heap solution
+# 20231203
 #########################
 from heapq import heappush, heappop
+from heapq import heappush, heappop, heappushpop
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        mxs = 0
+        mnq = []
+
+        for st, ed in intervals:
+            while mnq and st >= mnq[0]:
+                heappop(mnq)
+            heappush(mnq, ed)
+            mxs = max(mxs, len(mnq))
+        return mxs
+
+
+#########################
+# heap solution
+#########################
 
 
 class Solution:
