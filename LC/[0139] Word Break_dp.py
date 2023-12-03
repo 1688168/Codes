4b11c10af55e9ac5905b39c95530964510949e5d
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-03 16:57:27 -0500

    Update [0139] Word Break_dp.py

--- LC/[0139] Word Break_dp.py
+++ LC/[0139] Word Break_dp.py
@@ -1,6 +1,25 @@
 #############
+# 20231203
+#############
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        N = len(s)
+        word_set = set(wordDict)
+        dp = [False]*(N+1)
+        dp[0] = True
+
+        for ii in range(1, N+1):  # ii is the length
+            for jj in reversed(range(ii)):
+                if dp[jj] and s[jj:ii] in word_set:
+                    dp[ii] = True
+                if dp[ii]:
+                    break
+
+        return dp[-1]
+#############
 # 20231028
 #############
+
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
