01add5a4e71d554f7792adf705f611d84e259946
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-08 01:15:23 -0500

    Update [0297] Serialize and Deserialize Binary Tree - preorder v2.py

--- LC/[0297] Serialize and Deserialize Binary Tree - preorder v2.py
+++ LC/[0297] Serialize and Deserialize Binary Tree - preorder v2.py
@@ -1,5 +1,5 @@
 #######################
-# 20231126
+# 20231208
 #######################
 # Definition for a binary tree node.
 # class TreeNode(object):
@@ -9,6 +9,57 @@
 #         self.right = None
 
 from collections import deque
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if root is None:
+            return "#"
+
+        return str(root.val)+","+str(self.serialize(root.left))+","+str(self.serialize(root.right))
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        tkns = data.split(",")
+        nodes = collections.deque(
+            [TreeNode(t) if t != '#' else None for t in tkns])
+
+        def dfs(nodes):
+            curr = nodes.popleft()
+            if curr is None:
+                return None
+            left = dfs(nodes)
+            right = dfs(nodes)
+            curr.left = left
+            curr.right = right
+            return curr
+
+        return dfs(nodes)
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+#######################
+# 20231126
+#######################
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
 class Codec:
