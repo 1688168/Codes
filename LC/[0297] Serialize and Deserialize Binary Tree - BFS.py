ee1fe003326aff73e97efac8f97006564ba0792d
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-08 11:00:15 -0500

    Update [0297] Serialize and Deserialize Binary Tree - BFS.py

--- LC/[0297] Serialize and Deserialize Binary Tree - BFS.py
+++ LC/[0297] Serialize and Deserialize Binary Tree - BFS.py
@@ -1,4 +1,57 @@
 ################
+# 20231208
+################
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        dq = collections.deque([root])
+        res = ""
+        while (sz := len(dq)):
+            for _ in range(sz):
+                curr = dq.popleft()
+                if curr is None:
+                    res += "#,"
+                else:
+                    res += str(curr.val)+","
+                    dq.append(curr.left)
+                    dq.append(curr.right)
+
+        return res[:-1]
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        tkns = data.split(",")
+        nodes = [TreeNode(t) if t != '#' else None for t in tkns]
+        ii, jj, N = 0, 1, len(nodes)
+        while ii < N:
+            if nodes[ii] is not None:
+                nodes[ii].left = nodes[jj]
+                nodes[ii].right = nodes[jj+1]
+                jj += 2
+            ii += 1
+
+        return nodes[0]
+
+################
 # 20231126
 ################
 
@@ -8,8 +61,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
-from collections import deque
 
 
 class Codec:
