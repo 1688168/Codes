d490d92866f87bfddc1b12392548acb047b7540d
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-17 00:11:44 -0500

    881

--- LC/[0881] Boats to Save People - Greedy - Two Pointer.py
+++ LC/[0881] Boats to Save People - Greedy - Two Pointer.py
@@ -8,6 +8,25 @@ class Solution:
         1 2 2 
         """
         while ll <= rr:
+            if people[ll]+people[rr] <= limit:
+                ll += 1
+            rr -= 1
+            cnt += 1
+
+        return cnt
+
+
+######################
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        N = len(people)
+        ll, rr = 0, N-1
+        people.sort()
+        cnt = 0
+        """
+        1 2 2 
+        """
+        while ll <= rr:
             if people[ll]+people[rr] > limit:
                 rr -= 1
             else:
