4de4aa14ba231914a3428b0758e244b2582c6917
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-24 23:01:39 -0500

    881

--- LC/[0881] Boats to Save People - bucket sort - v2.py
+++ LC/[0881] Boats to Save People - bucket sort - v2.py
@@ -8,17 +8,19 @@ class Solution:
         cnt = 0
         x = limit
         y = 1
-        while x > 0:
+        while x > 0:  # visit each x from right
 
-            while x > 0 and p[x] == 0:
+            while x > 0 and p[x] == 0:  # find the first non-zero x
                 x -= 1
-            if x == 0:
+            if x == 0:  # get out when we visited all weights
                 break
 
-            cnt += 1
-            p[x] -= 1
+            cnt += 1  # for each non-zero x, we need a boat
+            p[x] -= 1  # set it to zero
+            # is there another ppl from left can fit in the boat?
             while x+y <= limit and p[y] == 0:
                 y += 1
+            # remove the 1st non-zero y that fits in the boat.
             if x+y <= limit and p[y] > 0:
                 p[y] -= 1
 
--- LC/[0881] Boats to Save People - bucket sort.py
+++ LC/[0881] Boats to Save People - bucket sort.py
@@ -1,16 +1,16 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        p = [0]*(limit+1)
+        p = [0]*(limit+1)  # each weight is a bucket
 
         for w in people:  # bucket sort
             p[w] += 1
 
         cnt = 0
         x = limit
-        while x > 0:
+        while x > 0:  # visit each weight
             jj = p[x]
 
-            while jj > 0:
+            while jj > 0:  # for each x, is there another y that can fit in same boat?
                 cnt += 1
                 p[x] -= 1
 
@@ -19,6 +19,7 @@ class Solution:
                 while y > 0 and p[y] == 0:
                     y -= 1  # all weights are greater than 0
                 if y > 0:
+                    # if we can fit in another ppl, remove it so it won't be counted again
                     p[y] -= 1
                 if x == y:
                     jj -= 1
