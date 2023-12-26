7c0685d83f4c3cd1f67427d9826e764970601f58
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-26 16:38:19 -0500

    Update [0300] Longest Increasing Subsequence - topdown memo.py

--- LC/[0300] Longest Increasing Subsequence - topdown memo.py
+++ LC/[0300] Longest Increasing Subsequence - topdown memo.py
@@ -1,4 +1,33 @@
+#############
+# 20231226
+#############
 from functools import lru_cache
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [[-1]*N for _ in range(N)]
+
+        def hp(prev, st):
+            if st >= N:
+                return 0
+
+            if dp[st][prev] != -1:
+                return dp[st][prev]
+
+            tke = 0
+            if prev == -1 or nums[st] > nums[prev]:
+                tke = 1+hp(st, st+1)
+
+            ntk = hp(prev, st+1)
+            dp[st][prev] = max(tke, ntk)
+            return dp[st][prev]
+
+        return hp(-1, 0)
+
+
+######################
 
 
 class Solution:
