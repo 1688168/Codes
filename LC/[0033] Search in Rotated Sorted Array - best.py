b20d39756da6c7ea8e15414c32516fa2372ad266
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-28 12:45:51 -0500

    Update [0033] Search in Rotated Sorted Array - best.py

--- LC/[0033] Search in Rotated Sorted Array - best.py
+++ LC/[0033] Search in Rotated Sorted Array - best.py
@@ -1,3 +1,37 @@
+###############
+# 20231228
+###############
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        1. search + sorted array -> binary search
+        2. N < 5k
+        3. logN ---> Binary Search
+        """
+        N = len(nums)
+        ll, rr = 0, N-1
+        ans = -1
+
+        while ll <= rr:
+            mm = ll+(rr-ll)//2
+
+            if nums[mm] == target:
+                return mm
+            if (nums[mm] < nums[0]) == (target < nums[0]):  # if same side
+                if nums[mm] < target:
+                    ll = mm+1
+                else:
+                    rr = mm-1
+            else:  # not same side
+                if nums[mm] < target:
+                    rr = mm-1
+                else:
+                    ll = mm+1
+        return ans
+
+####################
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         N = len(nums)
