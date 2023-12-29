0f935119bfb6a9151482ec564e8b133781af58e8
Y Lee <44484017+1688168@users.noreply.github.com>
2023-12-29 14:41:28 -0500

    2277

--- LC/[2277] Closest Node to Path in Tree.cpp
+++ LC/[2277] Closest Node to Path in Tree.cpp
@@ -14,6 +14,7 @@ class Solution {
 public:
     vector<int> closestNode(int n, vector<vector<int>>& edges, vector<vector<int>>& query) 
     {
+        //build the Graph
         for (auto& edge: edges) //build the graph
         {
             int a = edge[0], b = edge[1];
@@ -21,35 +22,40 @@ public:
             next[b].push_back(a);
         }
         
+        //build distance matrix
         for (int i=0; i<n; i++) //calc distance and populate the matrix
             dfs(i,i,0);
-                
-        //process queries
+        
+        // here we already built distance btn any two nodes
+        // process queries
         vector<int>rets;
         for (auto& q: query) //start to process the queries
         {
             int start = q[0], end = q[1], node = q[2];
-            int dist = INT_MAX;
+            int dist = INT_MAX; //default distance(node, path)
             int ret;
             
-            int cur = start;
-            while (1)
+            int cur = start; //first node on the path
+            while (1) //for a query
             {
-                if (matrix[cur][node] < dist)
+                if (matrix[cur][node] < dist) //if curr dist(node, curr) < dist -> update candidate
                 {
-                    dist = matrix[cur][node];
-                    ret = cur;
+                    dist = matrix[cur][node]; //update new shortest distance (node, curr_on_the_path)
+                    ret = cur; //update the node on the path with shortest distance (node, node_on_the_path)
                 }
-                if (cur==end) break;
+                //if curr==end, we are done
+                if (cur==end) break; //if start == end. the distance is btn start to node 
                 
-                for (int j: next[cur])
+                //identify next node on the path
+                for (int j: next[cur]) 
                 {
                     //this is a genius way to determin if child is on the path to end
                     if (matrix[cur][end]==matrix[j][end]+1) //if j is on the path, it must satisfy this
                     {
-                        cur = j;//since thi sis a tree (no cycle), we can only have one path
+                        cur = j;//since this is a tree (no cycle), we can only have one path
                         break; //once we identify which child in the path, we can break
                     }
+                    //here we found the next node on the path
                 }                
             }
             rets.push_back(ret);                        
--- LC/[2277] Closest Node to Path in Tree.md
+++ LC/[2277] Closest Node to Path in Tree.md
@@ -1,10 +1,16 @@
-> Solution I
+> Solution I: build distance matrix and
 
-1. build graph
+1. build graph/tree
 2. build matrix that records distance btn any two nodes.
    - per DFS. for each node, for each child
+   - or BFS (should be same performancewise we tree has no circle, only one path btn any two nodes)
 3. for each query
    - use matrix to check distance
+     a. default (node, path) distance as math.inf
+     b. from (node, start) and keep track min distance
+     c. find next node on path per: distance(curr, end) = 1+distance(curr_child, end)
+     d. once we find next child, see if (node, child) distance is shorter and update ans
+     e. exit state: if curr==end
    - find next node on the path and inspect if the distance is shorter
    - for formula to check if child is on the path
    - matrix[curr][end]==matrix[j][end]+1 [`Genius`]
