9a4e8a396323ec8230d316680d845a062715ce03
Y Lee <1688168@gmail.com>
2024-01-08 08:54:09 -0500

    Update [1268] Search Suggestions System - trie.py

--- LC/[1268] Search Suggestions System - trie.py
+++ LC/[1268] Search Suggestions System - trie.py
@@ -1,6 +1,25 @@
+"""
+# Problem Statements:
+1. Givne products and searchWord, 
+on each char entered from searchWord, return 3 min lexical ordered products
+
+# Solution 1: Trie
+1. define Trie
+2. build Trie from products
+  - define self.children=[None]*26 # for lexical ordering, 26 TrieNodes
+
+3. for each additional char into prefix
+  - BFS the Child Branch from current until we have 3 matches
+  
+# solution 2: bisect
+- sort the products
+- por each accumulated_prefix, bisect_left the sorted product
+- if next 3 (including current and less than N) elements share same prefix, output
+"""
+
 class TrieNode:
     def __init__(self):
-        self.children = [None]*26
+        self.children = [None]*26 #for the sake of lexical ordering
         self.is_word = False
 
 
@@ -37,27 +56,29 @@ class Solution:
         itr = trie.root
 
         def dfs(itr, path):
-            if len(matched) >= 3:
+            if len(matched) >= 3: # we only need top 3 results
                 return
 
             if itr.is_word:
                 matched.append(path)
 
-            for ii in range(26):
-                if itr.children[ii] is None:
+            for ii in range(26): # traverse all children
+                if itr.children[ii] is None: #ignore None child
                     continue
 
                 dfs(itr.children[ii], path+chr(ord('a')+ii))
-
+        # itr starts from root
         for ii, cc in enumerate(searchWord):
             prefix += cc
+
+            # if itr is unable to decend, -> no more search results possible
             if itr.children[ord(cc)-ord('a')] is None:
                 # all future search should return []
 
-                for jj in range(ii, N):
+                for jj in range(ii, N): # the remaining searchWord all get [] search results
                     res.append([])
                 break
-
+            # itr keeps decending on each additional search word
             itr = itr.children[ord(cc)-ord('a')]
             matched = []
             dfs(itr, prefix)
