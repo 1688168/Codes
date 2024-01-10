bf30e8c8153edc5e52aa07ad50ac249750197e50
Y Lee <44484017+1688168@users.noreply.github.com>
2024-01-10 01:22:53 -0500

    Update [1172] Design.py

--- LC/[1172] Design.py
+++ LC/[1172] Design.py
@@ -1,54 +1,80 @@
 from collections import deque
 from heapq import heappush, heappop, heappushpop
+from pprint import pprint as pp
+
+
 class DinnerPlates:
 
     def __init__(self, capacity: int):
-        self.dq=deque()
-        self.capacity=capacity
-        self.mnh=[] # the left most non-full stack index
-        self.mxh=[] #the right most non-empty stack index
-        
+        self.dq = deque()
+        self.capacity = capacity
+        self.mnh = []  # the left most non-full stack index
+        self.mxh = []  # the right most non-empty stack index
 
     def push(self, val: int) -> None:
-        if self.mnh: # the left most not-full stack
-            idx=heappop(self.mnh) # get the left most empty stack
-            self.dq[idx].append(val) # append new val
-            if len(self.dq[idx]) < self.capacity: #still not full
-                heappush(mnh, idx) # put it back to candidate
-        else: #we do not have existing stack that is not full
-            self.dq.append([val]) #append a new stack
+        if self.mnh:  # the left most not-full stack
+            idx = heappop(self.mnh)  # get the left most empty stack
+            self.dq[idx].append(val)  # append new val
+            if len(self.dq[idx]) < self.capacity:  # still not full
+                heappush(mnh, idx)  # put it back to candidate
+        else:  # we do not have existing stack that is not full
+            self.dq.append([val])  # append a new stack
 
-            idx=len(self.dq)-1 # the index of current stack
-            heappush(self.mnh, idx) # update the candidate stacks
-            heappush(self.mxh, -idx)# update the candidate of right most non-emapty
+            idx = len(self.dq)-1  # the index of current stack
+            if self.capacity > 1:
+                heappush(self.mnh, idx)  # update the candidate stacks
+            # update the candidate of right most non-emapty
+            heappush(self.mxh, -idx)
+
     def pop(self) -> int:
-        if len(self.dq)==0: return -1
-        if not self.mxh: return -1
-        right_most_non_empty_idx=(-heappop(self.mxh))
-        
-        if len(self.dq[right_most_non_empty_idx])==0: return -1 #shouldn't happpen
-        val=self.dq[right_most_non_empty_idx].pop()
+        print(" dq len: ", len(self.dq))
+        pp(self.dq)
+        print(" mnh: ", self.mnh)
+        print(" mxh: ", self.mxh)
+        if len(self.dq) == 0:
+            return -1
+        if not self.mxh:
+            return -1  # we don't have right most non-empty
+        right_most_non_empty_idx = (-heappop(self.mxh))
+
+        if len(self.dq[right_most_non_empty_idx]) == 0:
+            return -1  # shouldn't happpen
+        val = self.dq[right_most_non_empty_idx].pop()
 
         if len(self.dq[right_most_non_empty_idx]) > 0:
             heappush(self.mxh, -right_most_non_empty_idx)
 
-
         return val
-
 
     def popAtStack(self, index: int) -> int:
-        if index >= len(self.dq): return -1 # index exceeding max current stacks
+        if index >= len(self.dq):
+            return -1  # index exceeding max current stacks
 
-        stk=self.dq[index] # the current stack
-        if not stk: return -1 # if this guy is empty, return -1
-        
+        stk = self.dq[index]  # the current stack
+        if not stk:
+            return -1  # if this guy is empty, return -1
+
         # the target stack is NOT empty
-        heappush(self.mnh, index) # update left most not full heap
-        val=stk.pop() # get the value
-        
-        
+
+        try:
+            self.mnh.index(index)
+        except Exception as e:
+            heappush(self.mnh, index)  # update left most not full heap
+        val = stk.pop()  # get the value
+
+        if len(stk) == 0:
+            try:
+                print(" removing -index: ", index, " the mxh: ", self.mxh)
+
+                idx = self.mxh.index(-index)
+                print(" found index: ", idx)
+                del self.mxh[idx]
+                print(" after remove: ", self.mxh)
+                heapify(self.mxh)
+            except Exception as e:
+                print(" this shouldn't happen")
+
         return val
-        
 
 
 # Your DinnerPlates object will be instantiated and called as such:
