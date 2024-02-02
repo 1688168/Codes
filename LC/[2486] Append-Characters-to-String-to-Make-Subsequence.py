e67cdbcbae8720ad916fe1947a691c93b94346eb
Y Lee <1688168@gmail.com>
2024-02-02 08:11:52 -0500

    Update [2486] Append-Characters-to-String-to-Make-Subsequence.py

--- LC/[2486] Append-Characters-to-String-to-Make-Subsequence.py
+++ LC/[2486] Append-Characters-to-String-to-Make-Subsequence.py
@@ -1,3 +1,27 @@
+#############
+# 20240202
+#############
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        """
+        s:
+        t:
+        """
+        NS=len(s)
+        NT=len(t)
+        ii=jj=0
+
+        while ii < NS and jj < NT:
+            while ii < NS and s[ii]!=t[jj]:
+                ii+=1
+            if ii >= NS: break
+            ii+=1
+            jj+=1
+        
+        return NT-jj
+        
+
+###################
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
         ii, jj = 0, 0
