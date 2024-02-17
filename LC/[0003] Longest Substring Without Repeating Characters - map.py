b51c1acdc4dec9b905ca27ecb916d17f1ef89123
Y Lee <1688168@gmail.com>
2024-02-17 18:53:21 -0500

    Update [0003] Longest Substring Without Repeating Characters - map.py

--- LC/[0003] Longest Substring Without Repeating Characters - map.py
+++ LC/[0003] Longest Substring Without Repeating Characters - map.py
@@ -1,4 +1,34 @@
 #######
+# 20240217
+#######
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        two pointers and see the distance of pointer same as the unique count?
+        if less, shift the window until the window can continue to grow bigger
+        -> return the window size
+        """
+        ll=rr=0
+        N=len(s)
+        char2cnt=collections.defaultdict(int)
+        """
+        abcabcbb
+        """
+        if s=="": return 0
+        for rr in range(N):
+            dist=rr-ll+1
+            char2cnt[s[rr]] += 1
+            if dist > len(char2cnt): # has duplicate
+                char2cnt[s[ll]] -=1
+                if char2cnt[s[ll]] == 0: del char2cnt[s[ll]]
+                ll+=1
+        
+        return rr-ll+1
+        
+
+
+#######
 # 20230625
 #######
 
