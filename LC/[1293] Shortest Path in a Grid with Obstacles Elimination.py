2b65849b88df66d5872b5de869c5cde79b06bc8f
Y Lee <1688168@gmail.com>
2024-02-23 08:59:33 -0500

    Update [1293] Shortest Path in a Grid with Obstacles Elimination.py

--- LC/[1293] Shortest Path in a Grid with Obstacles Elimination.py
+++ LC/[1293] Shortest Path in a Grid with Obstacles Elimination.py
@@ -20,13 +20,12 @@ class Solution:
         while (sz:=len(dq)) > 0: #take sz for each level
             for _ in range(sz):# for each level
                 x, y, z = dq.popleft() #all nodes on same level, level starts from 0
-            
+          
+                if grid[x][y]==1:#if obstacle, consume one k
+                    if kk >= k: continue
+                    kk+=1
                 if x==M-1 and y==N-1: #arrived
                     return steps
-                if grid[x][y]==1:#if obstacle, consume one k
-                    if kk+1 > k: continue
-                    kk+=1
-               
                 visited.add((x, y, kk))
                 for dx, dy in dirs:
                     nx, ny = x+dx, y+dy
