f29e4b4ba25b5cf16824fbc86a161a4234816d75
Y Lee <1688168@gmail.com>
2024-02-25 15:54:09 -0500

    Update [2272] Substring With Largest Variance - v1.cpp

--- LC/[2272] Substring With Largest Variance - v1.cpp
+++ LC/[2272] Substring With Largest Variance - v1.cpp
@@ -1,3 +1,47 @@
+/*
+* 20240225
+*/
+/*
+    => largest variance for all substring in S
+    * for all char pairs (a, b) --- Time 26*26
+    * let a=1, b=-1, others=0 and calc max subarray sum
+    * max subarray sum -> modified Kadane (since need two chars)
+    => Overall Time: 26*26*N
+*/
+
+class Solution {
+public:
+    int largestVariance(string s) {
+        int n = s.size();//take size of the string
+        unordered_set<char> Set(s.begin(), s.end()); //add all chars in the string to set
+        int ret = 0;
+        for(char a: Set)
+            for(char b: Set){//26*26 combinations
+                if(a==b) continue; //need two chars
+                /* Type I DP: best use tmp var to avoid two vars interfering each other.  here the order matters and avoids the interferring
+                */
+                int dp0=0, dp1=INT_MIN/2; 
+                //now we have a pair of chars (a, b)
+                //(modified) kadane to find the max subarray sum
+                for(int ii=0; ii<n; ++ii){
+                    if(s[ii]==a){
+                        dp1 = dp1+1;
+                        dp0 = dp0 + 1; 
+                    }else if(s[ii]==b){ //here is else if as others are 0
+                        dp1 = max(dp0-1, dp1-1);
+                        dp0=0;
+                    }
+                    ret = max(ret, dp1);
+                }
+            }
+          
+        return ret;
+    }
+   
+};
+
+
+/* ------------ */
 class Solution {
 public:
     int largestVariance(string s)
