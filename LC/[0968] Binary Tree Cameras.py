0c9f04ede6b92d67f703b740b9e516a4c85da671
Y Lee <1688168@gmail.com>
2024-02-26 08:42:34 -0500

    Update [0968] Binary Tree Cameras.py

--- LC/[0968] Binary Tree Cameras.py
+++ LC/[0968] Binary Tree Cameras.py
@@ -4,28 +4,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-
 """
-Tree -> recursion
-current state is determined by neighboring state -> house robber -> DP
-Order matters: considering top down or bottom up
-Leaf-nodes are uncovered -> greedy
+* current state is relating to neighbor states -> house robber
+   -------------------------------
+   house robber     Tree Camera
+   -------------------------------
+   profit           count
+   rob/no rob       not coverred/has cam/no cam but coverred
+   array prev       post order traversal
+
+   * DP type I variation: house robber
+   * Greedy: heuristic on how to assign camera
+   * post-order recursion
+
+   # states
+     not covered         0
+     has camera          1
+     covered by neighbor 2
+
+
+   # considering leaf-nodes. 
+   -> Greedy: let leaf-node's parents has camera 
+   -> None nodes should return 2 to force leaf-nodes with state=0
 """
-
-
 class Solution:
-    def minCameraCover(self, root: TreeNode) -> int:
-        def solve(rt):
-            if not rt:  # base case if node is None
-                return 0, 0, float('inf')
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        cnt=0
+        def dfs(node):
+            nonlocal cnt
+            if node is None: return 2 # see comments above
 
-            L = solve(rt.left)
-            R = solve(rt.right)
+            left=dfs(node.left)
+            right=dfs(node.right)
 
-            dp0 = L[1]+R[1]  # uncovered
-            dp1 = min(L[2]+min(R[1:]), R[2]+min(L[1:]))  # with Camera
-            dp2 = 1+min(L)+min(R)  # without camera
-            return dp0, dp1, dp2
-
-        return min(solve(root)[1:])
+            if left==0 or right==0: #either child is not covered -> need add camera to cover
+                cnt += 1
+                return 1 # current must has camera in order to cover both children
+            
+            if left==2 and right==2: # both children are covered,  
+                return 0 # greedy, let parent camera to cover
+            
+            return 2 # either child has camera (1), ie, current is covered without a camera
+        root_state = dfs(root)
+        if root_state==0: # root is uncovered
+            cnt += 1
+        return cnt
