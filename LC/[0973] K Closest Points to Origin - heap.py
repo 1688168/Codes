9f34e431b63dc16cd6e4c81012d96e26ff89ff1f
Y Lee <1688168@gmail.com>
2024-03-29 16:34:18 -0400

    973

--- LC/[0973] K Closest Points to Origin - heap.py
+++ LC/[0973] K Closest Points to Origin - heap.py
@@ -2,6 +2,39 @@
 https://youtu.be/xi4QVECpmxQ
 
 """
+##########
+# 20240329
+##########
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        """
+        * N: 10^4
+        * points[ii]=[xi, yi]
+        * k
+        => topK smallest
+        0. sort: nlogn
+            - calc distance: N
+            - sort: nlogn
+            - n^2logn
+        1. heap
+            - calc distance: N
+            - heappush: logn
+        2. binary search
+            - 0~10^4: 32
+            - count(N)
+        3. Quick-Select: O(N): Avg
+        """
+        from heapq import heappush, heappop, heappushpop
+        # heap
+        mxh=[]
+        for x, y in points:
+            heappush(mxh, [-(pow(x,2)+pow(y,2)), (x, y)])
+            if len(mxh) > k: heappop(mxh)
+        
+        return [pp for dd, pp in mxh]
+
+
+########
 from heapq import heappush, heappop, heappushpop
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
