55ab7726413479e6bdaf5b3f1a0b015fbc26f5ed
Y Lee <44484017+1688168@users.noreply.github.com>
2024-04-03 09:30:52 -0400

    Update [1676] Lowest Common Ancestor of a Binary Tree IV.py

--- LC/[1676] Lowest Common Ancestor of a Binary Tree IV.py
+++ LC/[1676] Lowest Common Ancestor of a Binary Tree IV.py
@@ -1,3 +1,40 @@
+################
+# 20240403
+################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        children = set([v.val for v in nodes])
+
+        def helper(rt):
+            if rt is None:
+                return 0, None
+            lc, la = helper(rt.left)
+            if la is not None:
+                return 0, la
+            rc, ra = helper(rt.right)
+            if ra is not None:
+                return 0, ra
+
+            curr_cnt = lc + rc + (1 if rt.val in children else 0)
+
+            if curr_cnt == len(children):
+                return curr_cnt, rt
+            else:
+                return curr_cnt, None
+
+        cnt, ancestor = helper(root)
+
+        return ancestor
+
+
+###############################
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
