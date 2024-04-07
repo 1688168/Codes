e28e813ab66c1a268c33d182a42f6339ad0fd085
Y Lee <44484017+1688168@users.noreply.github.com>
2024-04-07 10:59:15 -0400

    Update [2323] Find Minimum Time to Finish All Jobs II.py

--- LC/[2323] Find Minimum Time to Finish All Jobs II.py
+++ LC/[2323] Find Minimum Time to Finish All Jobs II.py
@@ -1,6 +1,32 @@
 #############
+# 20240407
+#############
+class Solution:
+    def minimumTime(self, jobs: List[int], workers: List[int]) -> int:
+        """
+        jobs[ii]: time for job ii
+        workers[ii]: work hour for worker ii
+        N=10^5
+        => min num of days
+
+        ############
+        # bruteforce
+        1. N*(N-1)... => return min
+
+        # Greedy heuristic
+        """
+        jobs.sort()
+        workers.sort()
+        N = len(jobs)
+        required_days = 0
+        for ii in range(N):
+            required_days = max(required_days, math.ceil(jobs[ii]/workers[ii]))
+        return required_days
+#############
 # 20231007
 #############
+
+
 class Solution:
     def minimumTime(self, jobs: List[int], workers: List[int]) -> int:
         N = len(workers)
