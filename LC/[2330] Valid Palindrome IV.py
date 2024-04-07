9ad5900f4fbce4db1c492fabeabf3cd7cc9f2d61
Y Lee <44484017+1688168@users.noreply.github.com>
2024-04-07 10:49:31 -0400

    Update [2330] Valid Palindrome IV.py

--- LC/[2330] Valid Palindrome IV.py
+++ LC/[2330] Valid Palindrome IV.py
@@ -1,3 +1,31 @@
+
+###################
+# 20240407
+###################
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+        """
+        Greedy: ll, rr
+        => is palindrome
+        N=10^5
+        """
+        N = len(s)
+
+        is_palindrome = True
+        ll, rr = 0, N-1
+        cnt = 0
+        while ll < rr:
+            if s[ll] != s[rr]:
+                if cnt >= 2:
+                    return False
+                cnt += 1
+            ll += 1
+            rr -= 1
+
+        return is_palindrome
+################
+
+
 class Solution:
     def makePalindrome(self, s: str) -> bool:
         N = len(s)
@@ -5,7 +33,7 @@ class Solution:
         cnt = 0
         while ll < rr:
             if s[ll] != s[rr]:
-                cnt += 1
+                cnt += 1  # just break if cnt > 2
             ll += 1
             rr -= 1
 
