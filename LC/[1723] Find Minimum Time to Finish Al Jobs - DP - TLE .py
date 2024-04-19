9c8c509dfc7be593c959ec5e03119431c28d91f9
Y Lee <44484017+1688168@users.noreply.github.com>
2024-04-19 07:18:48 -0400

    Update [1723] Find Minimum Time to Finish Al Jobs - DP - TLE .py

--- LC/[1723] Find Minimum Time to Finish Al Jobs - DP - TLE .py
+++ LC/[1723] Find Minimum Time to Finish Al Jobs - DP - TLE .py
@@ -1,3 +1,42 @@
+#############
+# 20240419: some modification to reduce space
+#############
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
+        N = len(jobs)
+        SN = pow(2, N)
+        time = [0]*SN  # time[ii] is the required time to complete state[ii]
+        # 12+1 rows for initial state, each worker has 4096 states (jobs finished)
+        dp = [[math.inf]*SN for _ in range(k+1)]
+
+        # precalc time for each state
+        for state in range(1 << N):  # 0~2^N (not including)
+            ttl_time = 0
+            for ii in range(N):  # each state has N bits
+                if ((state >> ii) & 1) > 0:  # which bit we perform the work? accumulate the time
+                    ttl_time += jobs[ii]
+
+            time[state] = ttl_time
+
+        # special initail state: zero worker, zero jobs
+        dp[0][0] = 0
+
+        for ii in range(1, k+1):  # k workers given
+            for state in range(1 << N):  # the inner two loop time is 3^N, (not 2^N*2^N)
+                # iterate subset template: https://github.com/wisdompeak/LeetCode/blob/master/Template/Bit_manipulation/Iterate_Subsets.cpp
+                # given a state, how to traverse the subset?
+                # dp[ii][state]=math.inf
+                subset = state
+                while subset > 0:
+                    dp[ii][state] = min(dp[ii][state], max(
+                        dp[ii-1][state-subset], time[subset]))
+                    #      ^^^ done w/o you ^^^ ^^^ your subset ^^^
+                    subset = (subset-1) & state
+
+        return dp[-1][-1]
+
+
+########################
 class Solution:
     def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
         """
