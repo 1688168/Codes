d228eb41030aec7f5352171c83d7780ca0a40501
Y Lee <44484017+1688168@users.noreply.github.com>
2024-04-20 10:57:57 -0400

    Update [1723] Find Minimum Time to Finish Al Jobs - binary search and DFS by state - TLE.py

--- LC/[1723] Find Minimum Time to Finish Al Jobs - binary search and DFS by state - TLE.py
+++ LC/[1723] Find Minimum Time to Finish Al Jobs - binary search and DFS by state - TLE.py
@@ -1,3 +1,63 @@
+#############
+# 20240420: code refine
+#############
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
+        """
+        ? What's the min time among the workers who spend the max time to complete all jobs?
+        * Binary search the min time and see if feasible
+        * we are bruteforcing
+        """
+        # take the size of data
+        N = len(jobs)
+        NN = 1 << N
+
+        # precalc time
+        time = [0]*NN
+
+        for state in range(NN):
+            tt = 0
+            for ii in range(N):
+                if (state >> ii) & 1:
+                    tt += jobs[ii]
+            time[state] = tt
+
+        ll, rr = 1, sum(jobs)
+        ans = math.inf
+
+        def is_feasible(mm, state, kk):  # DFS searching if reachable?
+            if state == 0:
+                return True  # we finished all jobs without breaching threshold
+            if kk >= k:
+                return False  # we exceeded ttl workers still haven't finish all jobs
+            if (state, kk) in memo:
+                return False
+
+            subset = state
+            while subset:  # try all subsets
+                if time[subset] <= mm:
+                    if is_feasible(mm, state-subset, kk+1):
+                        return True
+
+                subset = state & (subset-1)
+
+            memo.add((state, kk))
+            return False
+
+        while ll <= rr:  # 32
+            mm = ll+(rr-ll)//2
+            # DFS's best friend
+            memo = set()
+            if is_feasible(mm, NN-1, 0):
+                ans = mm
+                rr = mm-1
+            else:
+                ll = mm+1
+
+        return ans
+#############################
+
+
 class Solution:
     def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
         time = [0]*4096  # time[ii] is the required time to complete state[ii]
