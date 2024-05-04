3352ed7421a0dd428d5a65b9ab3c57dc287e98ed
Y Lee <1688168@gmail.com>
2024-05-04 18:57:57 -0400

    Update [0432] All O`one Data Structure.cpp

--- LC/[0432] All O`one Data Structure.cpp
+++ LC/[0432] All O`one Data Structure.cpp
@@ -1,3 +1,95 @@
+/*
+ * 20240504
+*/
+class AllOne {  
+
+    //list to maintain order and support O(1) insert/delete operations
+    list<int> freq_list;
+
+    //kv update, given a key, mapping to it's frequency
+    unordered_map<string, int> key2freq; 
+
+    //from freq to key set
+    unordered_map<int, unordered_set<string>> freq2key_set; //given a frequency, mapping to a set of keys with this frequency
+
+    //freq to list iterator: given a frequency, mapping to the iterator pointing to the node in the list
+    unordered_map<int, list<int>::iterator> freq2iter;
+    
+
+public:
+    AllOne() {
+        freq_list.push_back(0);
+        freq2iter[0]=freq_list.begin();
+    }
+    
+    void inc(string key) {
+        //current freq
+        int freq = key2freq[key];//if key is not existing, we will get freq=0.  this is C++
+        int new_freq = freq+1;
+
+        //update key2freq
+        key2freq[key]=new_freq;
+        //remove from old set and add to new set
+        freq2key_set[new_freq].insert(key);
+        if(freq > 0)freq2key_set[freq].erase(key);
+        
+        if(freq2key_set[new_freq].size()==1){//we need a new node for this freq
+            freq_list.insert(next(freq2iter[freq]), new_freq);
+            freq2iter[new_freq]=next(freq2iter[freq]);
+        }
+
+        if(freq > 0 && freq2key_set[freq].size()==0){
+            freq_list.erase(freq2iter[freq]);
+        }
+        
+    }
+    
+    void dec(string key) {
+        //current freq
+        int freq = key2freq[key];//if key is not existing, we will get freq=0.  this is C++
+        if(freq==0) return; //unlike inc, we just bail
+        int new_freq = freq-1;
+        
+        //update key2freq
+        key2freq[key]=new_freq;
+
+        //remove from old set and add to new set
+        if(new_freq > 0)freq2key_set[new_freq].insert(key);
+        freq2key_set[freq].erase(key);//old freq must be existing otherwise we already returned from above
+        
+        if(new_freq > 0 && freq2key_set[new_freq].size()==1){//we need a new node for this freq
+            freq_list.insert(freq2iter[freq], new_freq);
+            freq2iter[new_freq]=prev(freq2iter[freq]);
+        }
+
+        if(freq2key_set[freq].size()==0){
+            freq_list.erase(freq2iter[freq]);
+        }
+    }
+    
+    string getMaxKey() {
+        if(freq_list.size()==1) return "";
+        return *(freq2key_set[freq_list.back()].begin());
+        
+    }
+    
+    string getMinKey() {
+        if(freq_list.size()==1) return "";
+        return *(freq2key_set[*(++freq_list.begin())].begin());
+        
+    }
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
+
+/* ===================================================== */
 class AllOne {
     list<int>List; // ordered sequence of values
     unordered_map<string,int>key2val;   // key->val
