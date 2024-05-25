88e74ac0d829869cc4fdb367a199db89c2e9fdaa
Y Lee <1688168@gmail.com>
2024-05-25 11:48:39 -0400

    Update [0309] Best Time to Buy and Sell Stock with Cooldown.py

---
+++ LC/[0309] Best Time to Buy and Sell Stock with Cooldown.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        N=len(prices)
+
+        bought=-prices[0]
+        sold=0 #-math.inf
+        cool_down=0 
+
+        for ii, pp in enumerate(prices[1:], 1):
+            bought_tmp=bought
+            sold_tmp=sold
+            cool_down_tmp=cool_down
+
+            bought = max(bought_tmp, cool_down_tmp-pp) #s.t. to cool down restriction
+            sold = max(bought_tmp+pp, sold_tmp)
+            cool_down = sold_tmp
+
+        return max(sold, 0)
