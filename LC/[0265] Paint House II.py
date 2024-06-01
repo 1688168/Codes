f4a3febbc4621312b6812c8a43a28c10cffae43e
Y Lee <1688168@gmail.com>
2024-06-01 16:04:54 -0400

    265

--- LC/[0265] Paint House II.py
+++ LC/[0265] Paint House II.py
@@ -1,3 +1,39 @@
+#############
+# 20240601
+#############
+from  heapq import heappush, heappop, heappushpop
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        N=len(costs)
+        K=len(costs[0])
+
+        dp=[[0]*K for _ in range(N)] #min cost ending @ color jj where 0<jj< kk
+
+        # initialize dp
+        mxq=[]
+        for jj in range(K): 
+            dp[0][jj]=costs[0][jj]
+            if len(mxq) < 2 or dp[0][jj] < -mxq[0][0]:
+                heappush(mxq, (-dp[0][jj], jj))
+            if len(mxq) > 2: heappop(mxq)
+
+        
+        for ii in range(1, N):
+            # find the top 2 min
+            min1_jj=mxq[1][1]
+            min2_jj=mxq[0][1]
+            mxq=[]
+            for jj in range(K):             
+                dp[ii][jj] = costs[ii][jj] + (dp[ii-1][min1_jj] if min1_jj != jj else dp[ii-1][min2_jj]) 
+                if len(mxq) < 2 or dp[ii][jj] < -mxq[0][0]:
+                    heappush(mxq, (-dp[ii][jj], jj))
+                if len(mxq) > 2: heappop(mxq)
+
+
+        return min(dp[-1])
+        
+
+#########################
 from  heapq import heappush, heappop, heappushpop
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
