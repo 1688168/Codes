37167774d78ed1e99e2092b0562f31ceaf7df314
Y Lee <1688168@gmail.com>
2024-06-05 00:24:24 -0400

    Update [2222] Number of Ways to Select Buildings - bruteforce - TLE .py

--- LC/[2222] Number of Ways to Select Buildings - bruteforce - TLE .py
+++ LC/[2222] Number of Ways to Select Buildings - bruteforce - TLE .py
@@ -1,3 +1,40 @@
+########
+# 20240605
+########
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        """
+        # I/O:
+        + s: '0' - office
+             '1' - restaurant
+        => number of valid ways to select 3 buildings
+
+        # Analysis:
+        + N=10^5 
+
+        > Bruteforce
+        > DP
+        """
+        N=len(s)
+        def dfs(prev, st, selected):
+            nonlocal ttl
+            
+            if selected==3:
+                ttl+=1
+                return
+            if st >=N: return
+            
+            for ii in range(st, N):
+                if prev is not None and s[ii] == s[prev]: continue
+                dfs(ii, ii+1, selected+1)
+        ttl=0
+        dfs(None, 0, 0)
+        return ttl
+        
+
+
+
+####################
 class Solution:
     def numberOfWays(self, s: str) -> int:
 
@@ -15,7 +52,7 @@ class Solution:
                 dfs(sz, ii+1, path+[s[ii]])
 
         N = len(s)
-        cnt = 0
+        cnt = 0     
         path = []
         dfs(3, 0, path)
         return cnt
