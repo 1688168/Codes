23cd7cdff6c75fa2042e32cc2a6414eb3ef13624
Y Lee <1688168@gmail.com>
2024-06-15 09:28:38 -0400

    Update [0474] Ones and Zeroes - BottomUp - 3D DP.py

--- LC/[0474] Ones and Zeroes - BottomUp - 3D DP.py
+++ LC/[0474] Ones and Zeroes - BottomUp - 3D DP.py
@@ -1,3 +1,41 @@
+################
+# 20240615
+################
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        """
+        * single array with limitations m, n
+        -> size of largest subset
+        * subset -> take or skip
+        """
+        s2cnt=[] # (zero_cnt, one_cnt )
+        for ss in strs:
+            zero=0
+            one=0
+            for cc in ss:
+                if cc=='0': zero +=1
+                if cc=='1': one += 1
+            
+            s2cnt.append((zero, one))
+        
+        N=len(strs)
+        dp=[[[0]*(n+1) for _ in range(m+1)] for _ in range(N)]
+
+        for ii in range(N): #ii is the idx of nums
+            zz, oo = s2cnt[ii]
+            for jj in range(m+1): # jj is number of zero
+                for kk in range(n+1): # kk is number of ones
+                    if ii==0:
+                        if jj >= zz and kk >= oo: dp[ii][jj][kk]=1
+                        continue
+                    #skip
+                    dp[ii][jj][kk] = max(dp[ii][jj][kk], dp[ii-1][jj][kk])
+
+                    #take               
+                    if jj-zz >=0 and kk-oo >=0:
+                        dp[ii][jj][kk] = max(dp[ii][jj][kk], dp[ii-1][jj-zz][kk-oo]+1)
+        return dp[-1][-1][-1]
+###############################
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         N=len(strs)
