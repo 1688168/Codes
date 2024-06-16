89739041e6975d5d1feaa28bd002919c24f102bd
Y Lee <1688168@gmail.com>
2024-06-16 10:37:46 -0400

    Create [0075] Sort Colors.cpp

---
+++ LC/[0075] Sort Colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int N=nums.size();
+        int ii=0, jj=0, kk=N-1;
+        while(jj<=kk){
+            if(nums[jj]< 1){
+                swap(nums[ii], nums[jj]);
+                ++ii;
+                ++jj;
+            }else if (nums[jj]==1){
+                ++jj;
+            }else{
+                swap(nums[jj], nums[kk]);
+                --kk;
+            }
+
+        }
+        
+    }
+};
