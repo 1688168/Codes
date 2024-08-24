abd4e8bc770d7da25577f8c2ade2359ec938a59f
Y Lee <1688168@gmail.com>
2024-08-23 21:10:06 -0400

    Update [0474] Ones and Zeroes - BottomUp - 2D DP.cpp

--- LC/[0474] Ones and Zeroes - BottomUp - 2D DP.cpp
+++ LC/[0474] Ones and Zeroes - BottomUp - 2D DP.cpp
@@ -1,3 +1,34 @@
+// 20240823
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int N = strs.size();
+        auto dp = vector<vector<int>>(m+1, vector<int>(n+1, 0));
+
+        for(int ii=0; ii<N; ++ii){
+            int zz=0;
+            int oo=0;
+            //count zero/ones
+            for(auto x: strs[ii]){
+                if(x=='0'){
+                    ++zz;
+                }else{
+                    ++oo;
+                }       
+            }
+
+            auto tmp = dp;
+            for(int jj=zz; jj<=m; ++jj){
+                for(int kk=oo; kk<=n; ++kk){
+                    dp[jj][kk] = max(tmp[jj][kk], tmp[jj-zz][kk-oo]+1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+//=========================
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) 
