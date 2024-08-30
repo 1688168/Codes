920ebc3b00709abecd6f4bd2315acf3ac598ff49
Y Lee <44484017+1688168@users.noreply.github.com>
2024-08-30 12:26:44 -0400

    Update [0956] Tallest Billboard.py

--- LC/[0956] Tallest Billboard.py
+++ LC/[0956] Tallest Billboard.py
@@ -2,11 +2,13 @@ class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
         offset = 5000
         dp=[-1]*(2*offset+1) #[-5000, 5000], "-1" indicate no left/right can make the diff
+        dp_old=[-1]*(2*offset+1)
         dp[0+offset]=0
-        N=len(rods)
+    
         for ii, ll in enumerate(rods): # [0, N-1] - for each rod
-            dp_old = copy.deepcopy(dp)
-            for diff in range(-offset, offset+1, 1):
+            dp_old = deepcopy(dp)
+            #print("old: ", dp_old)
+            for diff in range(-1*offset, offset+1, 1):
                 if dp_old[diff+offset]==-1: continue
                 # add to left
                 if diff+ll<=offset:         
@@ -14,6 +16,6 @@ class Solution:
 
                 # add to right
                 if diff-ll >= -offset:
-                    dp[diff-ll+offset] = max(dp[diff-ll+offset], dp_old[diff+offset]-ll)
-
+                    dp[diff-ll+offset] = max(dp[diff-ll+offset], dp_old[diff+offset])
+        
         return dp[0+offset]
