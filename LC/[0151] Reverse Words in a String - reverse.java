95fdac1d01d98533faa86b08ad5fbe4857939ad6
Y Lee <1688168@gmail.com>
2024-08-31 11:05:07 -0400

    Update [0151] Reverse Words in a String - reverse.java

--- LC/[0151] Reverse Words in a String - reverse.java
+++ LC/[0151] Reverse Words in a String - reverse.java
@@ -1,8 +1,8 @@
 // 20240831 -----------
 class Solution {
     public String reverseWords(String s) {
-        String[] ss = s.replaceAll("\\s+", " ").trim().split(" ");
-        Collections.reverse(Arrays.asList(ss));        
-        return String.join(" ", Arrays.asList(ss)); 
+        String[] ss = s.replaceAll("\\s+", " ").trim().split(" "); //regex
+        Collections.reverse(Arrays.asList(ss)); //reverse list of string
+        return String.join(" ", Arrays.asList(ss)); //join a list of string, concatenate list of string
     }
 }
