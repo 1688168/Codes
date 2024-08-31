a53f7169fa0e7ef4fe25148a6c794c67ba25c744
Y Lee <1688168@gmail.com>
2024-08-30 22:45:37 -0400

    Update [0075] Sort Colors.java

--- LC/[0075] Sort Colors.java
+++ LC/[0075] Sort Colors.java
@@ -1,3 +1,29 @@
+//20240830
+class Solution {
+    public void sortColors(int[] nums) {
+        int N=nums.length;
+        int ii=0, jj=0, kk=N-1;
+        int tmp=0;
+        while(jj<=kk){
+            if(nums[jj]==0){
+                tmp=nums[jj];
+                nums[jj]=nums[ii];
+                nums[ii]=tmp;
+                ++ii;
+                ++jj;
+            }else if(nums[jj]==1){
+                ++jj;
+            }else{
+                tmp=nums[kk];
+                nums[kk]=nums[jj];
+                nums[jj]=tmp;
+                --kk;
+            }
+        }
+    }
+}
+
+//---------------
 class Solution {
     private void swap(int ii, int jj, int[] nums){//java swap
         int tmp = nums[ii];
