0c4604f5548332ee54c33910dc940136c6ebd750
Y Lee <44484017+1688168@users.noreply.github.com>
2024-09-05 08:04:49 -0400

    Update [0956] Tallest Billboard - non_contribution_model.java

--- LC/[0956] Tallest Billboard - non_contribution_model.java
+++ LC/[0956] Tallest Billboard - non_contribution_model.java
@@ -1,33 +1,33 @@
 class Solution {
     public int tallestBillboard(int[] rods) {
         int N = rods.length;
-        int offset=5000;
-        int[] dp = new int[offset*2+5];//rods=[0, N], diff=[-5k, 5k]
-     
-        //initialize int array to a specific value
-        dp=Arrays.stream(dp).map(x -> -1).toArray();
-       
-        //initialize DP
-        dp[0+offset]=0;//start from maz height as zero when we have nothing
+        int offset = 5000;
+        int[] dp = new int[offset * 2 + 5];// rods=[0, N], diff=[-5k, 5k]
 
-        //int [] dp_old = new int[offset*2+5];
-        for(int ii=0; ii<N; ++ii){//this is contribution model, so in range [0, N-1] - all rods
-            //clone the old dp
-            //IntStream.range(0, N+1).map(idx -> dp_old[idx]=dp[idx]);
-            int[] dp_old = dp.clone(); //Java how to copy array
-            int ll=rods[ii];
+        // initialize int array to a specific value
+        dp = Arrays.stream(dp).map(x -> -1).toArray();
 
-            for(int jj=-offset; jj<offset; ++jj){//we don't need to contribute in end last use case
-                if(jj+ll <= offset && dp_old[jj+ll+offset] != -1){ //added left from old
-                    dp[jj+ll+offset] = Math.max(dp[jj+ll+offset], dp_old[jj+offset]+ll);
+        // initialize DP
+        dp[0 + offset] = 0;// start from maz height as zero when we have nothing
+
+        // int [] dp_old = new int[offset*2+5];
+        for (int ii = 0; ii < N; ++ii) {
+            // clone the old dp
+            // IntStream.range(0, N+1).map(idx -> dp_old[idx]=dp[idx]);
+            int[] dp_old = dp.clone(); // Java how to copy array
+            int ll = rods[ii];
+
+            for (int jj = -offset; jj <= offset; ++jj) {// we don't need to contribute in end last use case
+                if (jj - ll >= -offset && dp_old[jj - ll + offset] != -1) { // added left from old
+                    dp[jj + offset] = Math.max(dp[jj + offset], dp_old[jj - ll + offset]+ll);
                 }
-                      
-                if(jj-ll >=-offset && dp_old[jj-ll+offset] != -1){
-                    dp[jj-ll+offset] = Math.max(dp[jj-ll+offset],dp_old[jj+offset]);
-                }       
-            }  
+
+                if (jj + ll <= offset && dp_old[jj + ll + offset] != -1) {
+                    dp[jj + offset] = Math.max(dp[jj + offset], dp_old[jj + ll + offset]);
+                }
+            }
         }
 
-        return dp[0+offset];//max height for diff=0
+        return dp[0 + offset];// max height for diff=0
     }
 }
