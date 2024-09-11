cc43028aa4689b31567ef2f7c106d8a68ddf435c
Y Lee <44484017+1688168@users.noreply.github.com>
2024-09-11 07:58:27 -0400

    Update [1049] Last Stone Weight II - set.java

--- LC/[1049] Last Stone Weight II - set.java
+++ LC/[1049] Last Stone Weight II - set.java
@@ -1,3 +1,49 @@
+// 20240911
+class Solution {
+    /*
+    Problem: given a list of stones, cancel/merge any two until only one or none last.
+    Asking: the remaining weight after all cancel/merge
+    Observation: eventually the cancel/merge process can be represented as: 
+    -> ((B-A)-C)-(D-E))
+    -> flattening
+    -> B-A-C-D+E
+    -> (B+E)  -  (C+D)
+       bigger     smaller
+    => partition to two groups. 
+    => sum(bigger)-sum(smaller) ~= 0 
+    => sum(smaller) <= total/2
+    => knapsack problem: pick/skip element to smaller_group with capacity=total//2
+    => dp[ii][jj] = max total weight for smaller group using up to iith stone with capacity jj
+    => return total-2*dp[-1][capacity]
+
+    # Solution II:
+    * bigger-smaller
+    = subsetA-subsetB
+    = a+b+c+...-x-y-z
+
+    -> we will have each element being assigned with + or -
+    -> if we can construct all potential result and return the smallest one that is greater than equal to zero
+    */
+    public int lastStoneWeightII(int[] stones) {
+        Set<Integer> set = new HashSet<Integer>(Arrays.asList(Integer.valueOf(0)));
+      
+        for(var ss :stones){//for each stone
+            Set<Integer> oset = new HashSet<>(set);
+            set.clear();
+            //collecting all possible final combination
+            for(var s: oset){//for each final num.
+                set.add(s+Integer.valueOf(ss));
+                set.add(s-Integer.valueOf(ss));
+            }
+        }
+        int ans=Integer.MAX_VALUE;
+        for(var x: set){
+            ans = (x >=0 && x<ans)? x:ans;
+        }
+        return ans;
+    }
+}
+//---------------------------------
 class Solution {
     /*
     Problem: given a list of stones, cancel/merge any two until only one or none last.
