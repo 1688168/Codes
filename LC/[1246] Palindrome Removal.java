78b2839958e5f55aa07b9a537549251d8d1204e8
Y Lee <1688168@gmail.com>
2024-11-23 12:01:45 -0500

    Update [1246] Palindrome Removal.java

--- LC/[1246] Palindrome Removal.java
+++ LC/[1246] Palindrome Removal.java
@@ -4,39 +4,24 @@ class Solution {
         int N=arr.length;
         
         if(N==1) return 1;
+        //Java insert dummy in the beginning of the array
+        //stream->list->insert->array
 
-/**
         //convert int array to Integer Array List
-        List<Integer> list = IntStream.of(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
-        //insert dummy in the beginning of nums
+        List<Integer> list = IntStream.of(arr).boxed().collect(Collectors.toCollection(ArrayList::new));
         list.add(0, 0);
- */
-
-        //var tmp = IntStream.of(arr).boxed().collect(Collectors.toCollection(ArrayList::new)).add(0, 0);
-
-
-        var arr2 = new ArrayList<Integer>();
-        //java insert dummy to array
-        arr2.add(0);
-        arr2.addAll(Arrays.stream(arr).boxed().collect(Collectors.toList()));
-        arr=arr2.stream().mapToInt(i->i).toArray();
+        arr=list.stream().mapToInt(ii->ii).toArray();
 
         int[][] dp = new int[N+2][N+2];
  
-  
         //initialize dp
         for(int len = 1; len<=N; ++len){
             for(int ii=1; ii+len-1<=N; ++ii){//beginning index
                 int jj=ii+len-1; //ending index
                 dp[ii][jj] = Integer.MAX_VALUE;
-                for(int kk=ii; kk<=jj; ++kk){//partitioning the interval,kk is the partition point
-                    if(arr[kk]==arr[jj]){
-                        /* -- how do you transition from big interval to smaller interval
-                        partition range [ii, jj] to [ii, kk-1][kk, jj]
-                        */
-
+                for(int kk=ii; kk<=jj; ++kk){//try each kk in [ii:jj-1] and see if we can eliminate both in one shot
+                    if(arr[kk]==arr[jj]){ //worst case kk==jj -> arr[kk]==arr[jj] -> remove the last one
                         dp[ii][jj] = Math.min(dp[ii][jj], dp[ii][kk-1]+ Math.max(1,dp[kk+1][jj-1]));
-                        //dp[ii][jj] = Math.min(dp[ii][jj], dp[ii][kk-1]+ dp[kk+1][jj-1]);
                     }
                 }
             }
