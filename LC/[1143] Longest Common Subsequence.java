78a72537f71956a2e82a577030dac8fb72e6faea
Y Lee <44484017+1688168@users.noreply.github.com>
2025-03-03 08:22:46 -0500

    Update [1143] Longest Common Subsequence.java

---
+++ LC/[1143] Longest Common Subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int N1 = text1.length();
+        int N2 = text2.length();
+
+        String ss="#"+text1;
+        String tt="#"+text2;
+
+        var dp = IntStream
+                .range(0, N1+1)
+                .mapToObj(ii -> IntStream.range(0, N2+1).map(jj->0).toArray())
+                .toArray(int[][]::new);
+
+        for(int ii=1; ii<=N1; ++ii){
+            for(int jj=1; jj<=N2; ++jj){
+                if(ss.charAt(ii)==tt.charAt(jj)){
+                    dp[ii][jj] = dp[ii-1][jj-1] + 1;
+                }else{
+                    dp[ii][jj] = Math.max(dp[ii-1][jj], dp[ii][jj-1]);
+                }
+            }
+        }
+
+
+        return dp[N1][N2];
+    }
+}
