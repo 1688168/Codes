a711a89a920ca7ff4f66e2cee015f8afc0e69803
Y Lee <44484017+1688168@users.noreply.github.com>
2025-05-07 08:27:44 -0400

    Update [1959] Minimum Total Space Wasted With K Resizing Operations.java

--- LC/[1959] Minimum Total Space Wasted With K Resizing Operations.java
+++ LC/[1959] Minimum Total Space Wasted With K Resizing Operations.java
@@ -3,8 +3,8 @@ class Solution {
         //min waste given an array and cost formula
         //for each ii, try all the valid size (color)
         //1473 color is given, 1959, valid size is all the max upto ii <<<<< 
-        //1473 try all the colors
-        //1959 try all valid size
+        //1473 try all the colors -> this is a type I DP
+        //1959 try all valid size -> this is an interval DP. last interval on top of prior interval
 
         int N = nums.length;
 
@@ -20,25 +20,30 @@ class Solution {
                 dp[ii][jj]= Integer.MAX_VALUE/2;
         }
 
-        //first item does NOT count as resize
+        //when jj=0 -- we have jj start from 1 -> no reset -> min waste is have sz=mx in the interval 
+        int mx=0;
+        int ttl=0;
+        for(int ii=0; ii<N; ++ii){
+            mx=Math.max(mx, nums[ii]);
+            ttl += nums[ii];
+            dp[ii][0] = mx*(ii+1)-ttl;
+        }
+
         dp[0][0]=0;
-
-        //if no resize -> size is always first element
-
         //populate dp
         for(int ii=1; ii<N; ++ii){//for each item in array
             for(int jj=1; jj<=Math.min(ii, k); ++jj){ //for each resize count, cannot reset more than available ii
-                int mx=0;
-                int ttl=0;
+                mx=0;
+                ttl=0;
                 for(int st=ii; st>=1; --st){
-                    ttl += nums[ii];
-                    mx=Math.max(mx, nums[ii]);
+                    ttl += nums[st];
+                    mx=Math.max(mx, nums[st]);
                     dp[ii][jj] = Math.min(dp[ii][jj], dp[st-1][jj-1] + mx*(ii-st+1)-ttl);
                 }
             }
         }
 
-        int ret=0;
+        int ret=Integer.MAX_VALUE/2;
 
         for(int jj=0; jj<=k; ++jj) ret = Math.min(ret, dp[N-1][jj]);
 
