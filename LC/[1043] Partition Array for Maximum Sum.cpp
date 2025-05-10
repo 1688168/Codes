f92d8faa585718bc434dbf2ee16ce01b979c7a61
Y Lee <1688168@gmail.com>
2025-05-10 01:11:09 -0400

    Update [1043] Partition Array for Maximum Sum.cpp

--- LC/[1043] Partition Array for Maximum Sum.cpp
+++ LC/[1043] Partition Array for Maximum Sum.cpp
@@ -5,16 +5,15 @@ class Solution {
             //declare/define DP
             vector<vector<int>> dp = vector(N, vector(k+1, INT_MIN/2));
             //initialize DP base cases
-            //when ii=0
-            dp[0][1] = arr[0];
+            //when ii=0, jj=0
+            dp[0][0] = 0;
     
             //when jj=0 //no partition
             for(int ii=1; ii<N; ++ii) dp[ii][0] = 0;
     
             //when jj=1 //one partition
-    
             int mx = 0;
-            for(int ii=1; ii<N; ++ii){
+            for(int ii=0; ii<N; ++ii){
                 mx=max(mx, arr[ii]);
                 dp[ii][1] = mx*(ii+1);
             }
@@ -22,16 +21,17 @@ class Solution {
             //populate DP
             for(int ii=1; ii<N; ++ii){//for each element
                 for(int jj=2; jj<=min(ii, k); ++jj){//for each partition
-                    int mx=0;
-                    int ttl=0;
+                    int mx=0;      
                     for(int ss=ii; ss>=1; --ss){ //for each interval start
-                        mx=max(mx, arr[ss]);
-                        ttl += arr[ss];
+                        mx=max(mx, arr[ss]);          
                         dp[ii][jj] = max(dp[ii][jj], dp[ss-1][jj-1]+mx*(ii-ss+1));
                     }
                 }
             }  
     
-            return dp[N-1][k];
+            int ret = 0;
+            for(int jj=0; jj<=k; ++jj) ret = max(ret, dp[N-1][jj]);
+    
+            return ret;
         }
     };
