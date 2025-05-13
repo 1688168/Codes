26b7163597cf57b537c4ef798272d6a7a7ced7a1
Y Lee <44484017+1688168@users.noreply.github.com>
2025-05-13 08:04:11 -0400

    Update [1105] Filling Bookcase Shelves.java

---
+++ LC/[1105] Filling Bookcase Shelves.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int N = books.length;
+        int [] dp = new int[N+1];
+        int k = shelfWidth;
+
+        //insert dummy
+        int[][] books2 = new int[N+1][2];
+        for(int ii=0; ii<N; ++ii){
+            books2[ii+1]=books[ii];
+        }
+
+        books = books2;
+        //ii=0 (dummy head)
+        dp[0]=0;
+        //initialize dp
+        for(int ii=1; ii<=N; ++ii) dp[ii] = Integer.MAX_VALUE/2;
+
+        //populate dp
+        for(int ii=1; ii<=N; ++ii){//for each book
+            int curr_thickness=0;
+            int mx=0;
+            for(int ss=ii; ss>=1; --ss){
+                curr_thickness += books[ss][0];
+                mx = Math.max(mx, books[ss][1]);
+                if(curr_thickness > k) break;
+                dp[ii] = Math.min(dp[ii], dp[ss-1]+mx);
+            }
+
+        }
+
+        return dp[N];
+    }
+}
