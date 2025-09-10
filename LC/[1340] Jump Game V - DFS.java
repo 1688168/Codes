7fb9f277208b94597b6bc6b0d1ffcffde4e6c97b
Y Lee <1688168@gmail.com>
2025-09-09 23:55:23 -0400

    Update [1340] Jump Game V - DFS.java

--- LC/[1340] Jump Game V - DFS.java
+++ LC/[1340] Jump Game V - DFS.java
@@ -1,34 +1,36 @@
-import java.util.*;
-
 class Solution {
-    public int maxJumps(int[] arr, int d) {
-        int n = arr.length;
-        int[] memo = new int[n]; // 0 means uncomputed
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, dfs(i, arr, d, memo));
+    private int dfs(int idx, Map<Integer, Integer> idx2cntMemo, int D, int[] arr){
+        int N=arr.length;
+        if(idx2cntMemo.containsKey(idx)) return idx2cntMemo.get(idx);
+
+        int res=1;//default is itself.
+        //jumping left
+        for(int jj=idx-1; jj>=Math.max(0, idx-D); --jj){
+            if(arr[jj] >= arr[idx]) break;
+            res=Math.max(res, 1+dfs(jj, idx2cntMemo, D, arr));
         }
-        return ans;
+
+        //jumping right
+        for(int jj=idx+1; jj<Math.min(N, idx+D+1); ++jj){
+            if(arr[jj] >= arr[idx]) break;
+            res=Math.max(res, 1+dfs(jj, idx2cntMemo, D, arr));
+        }
+
+        idx2cntMemo.put(idx, res);
+        return res;
+
     }
+    public int maxJumps(int[] arr, int d) {
+        int N=arr.length;
 
-    private int dfs(int i, int[] arr, int d, int[] memo) {
-        if (memo[i] != 0) return memo[i];
-        int n = arr.length;
-        int best = 1;
+        Map<Integer, Integer> idx2cntMemo = new HashMap<>();
 
-        // Explore to the right
-        for (int j = i + 1; j <= Math.min(n - 1, i + d); j++) {
-            if (arr[j] >= arr[i]) break;         // blocked by equal/higher
-            best = Math.max(best, 1 + dfs(j, arr, d, memo));
+        //we need to try each index 
+        int res=0;
+        for(int ii=0; ii<N; ++ii){
+            res=Math.max(res, dfs(ii, idx2cntMemo, d, arr));
         }
 
-        // Explore to the left
-        for (int j = i - 1; j >= Math.max(0, i - d); j--) {
-            if (arr[j] >= arr[i]) break;         // blocked by equal/higher
-            best = Math.max(best, 1 + dfs(j, arr, d, memo));
-        }
-
-        memo[i] = best;
-        return best;
+        return res;
     }
 }
