84c28f9ca39252948acf609a33b7f57b551e323c
Y Lee <1688168@gmail.com>
2025-09-15 23:33:15 -0400

    1871

---
+++ LC/[1871] Jump Game VII - bruteforce - TLE.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        N=len(s)
+
+        if s[0] != '0' or s[-1] != '0': return False
+
+        reachable=[False]*N
+        reachable[0]=True
+
+        for ii, vv in enumerate(s):
+            #print("ii: ", ii, " reachable: ", reachable)
+        
+            if not reachable[ii] or vv!='0': continue
+            
+            min_next = ii+minJump
+            max_next = min(ii+maxJump, N-1)
+
+            for jj in range(min_next, max_next+1):
+                if s[jj]=='0': reachable[jj]=True
+            if reachable[-1]: return True
+
+        return reachable[-1]
---
+++ LC/[1871] Jump Game VII.md
@@ -0,0 +1,8 @@
+> Why we cannot use greedy algorithm for Jump Game VII.
+> Considering Greedy.   Jump Game I (0055) VS Jump Game VII (1871)
+
+* in Jump Game I: we have no limitation on which index is jumpable and no limitation on minJump.  That being the case, every index can be the starting point for the next jump.
+* Considering Jump Game VII: we have minJump limitation.  so starting point become critical.  cuz we potentially could reach a JJ but because of minJump limitation, we could jump over the end.  Also, JJ could be an none "0" which cannot even be a index to jump from.
+
+> Review Jump Game I (0055)
+> Strategy of Jump Game VII
