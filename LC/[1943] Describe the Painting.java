4eaa032ef38570d3674c5e2b56e6e7c0e00c4de2
Y Lee <1688168@gmail.com>
2025-11-21 21:50:06 -0500

    1943

---
+++ LC/[1943] Describe the Painting.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Long>> splitPainting(int[][] segments) {
+
+        // TreeMap = ordered map (same as C++ map)
+        TreeMap<Long, Long> events = new TreeMap<>();
+
+        for (int[] seg : segments) {
+            long l = seg[0], r = seg[1], c = seg[2];
+            events.put(l, events.getOrDefault(l, 0L) + c);
+            events.put(r, events.getOrDefault(r, 0L) - c);
+        }
+
+        List<List<Long>> rets = new ArrayList<>();
+        long cnt = 0;
+        long start = -1, end = -1;
+
+        for (Map.Entry<Long, Long> event : events.entrySet()) {
+            long pos = event.getKey();
+            long diff = event.getValue();
+
+            if (start == -1) {
+                start = pos;
+            } else {
+                end = pos;
+                if (cnt != 0) { // avoid adding zero-color segment
+                    rets.add(Arrays.asList(start, end, cnt));
+                }
+                start = end;
+            }
+
+            cnt += diff;
+
+            if (cnt == 0) start = -1;
+        }
+
+        return rets;
+    }
+}
---
+++ LC/[1943] Describe the Painting.py
@@ -0,0 +1,34 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
+        events = defaultdict(int)   # position -> delta color sum
+
+        for seg in segments:
+            l, r, c = seg
+            events[l] += c
+            events[r] -= c
+
+        rets: List[List[int]] = []
+        cnt = 0
+        start = -1
+        end = -1
+
+        for pos in sorted(events.keys()):
+            diff = events[pos]
+
+            if start == -1:
+                start = pos
+            else:
+                end = pos
+                if cnt != 0:  # avoid zero-color segments
+                    rets.append([start, end, cnt])
+                start = end
+
+            cnt += diff
+
+            if cnt == 0:
+                start = -1
+
+        return rets
