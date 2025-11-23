f78b0f1cdf22db7f55a7c9388b92f0ac71cba1c7
Y Lee <1688168@gmail.com>
2025-11-23 12:38:45 -0500

    1943

--- LC/[1943] Describe the Painting.py
+++ LC/[1943] Describe the Painting.py
@@ -1,33 +1,42 @@
-from typing import List
 from collections import defaultdict
-
 class Solution:
     def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
-        events = defaultdict(int)   # position -> delta color sum
+        N=len(segments) # how many intervals
+        events = defaultdict(int) #default value to int
 
-        for seg in segments:
-            l, r, c = seg
-            events[l] += c
-            events[r] -= c
+        # converting intervals to events (ts, cnt)
+        for st, ed, ct in segments:
+            events[st] = events.get(st, 0) + ct
+            events[ed] = events.get(ed, 0) - ct
 
-        rets: List[List[int]] = []
-        cnt = 0
-        start = -1
-        end = -1
 
-        for pos in sorted(events.keys()):
-            diff = events[pos]
+        res=[]
+        acc=0
+        st=-1
+        for ts, cnt in sorted(events.items(), key=lambda x: x[0]):
+            if st==-1: #we have a new start
+                st=ts
+            else: #we have an end
+                res.append([st, ts, acc])
+                st=ts
+            acc += cnt
+            if acc==0: st=-1
 
-            if start == -1:
-                start = pos
-            else:
-                end = pos              
-                rets.append([start, end, cnt])
-                start = end
+        return res
 
-            cnt += diff
+"""
+# Given
+* intervals (overlapping, non-inclusive) with unique colors
+* implying after merging, the color still unique (not very clear on 
+this, but it is working)
 
-            if cnt == 0:
-                start = -1
+# Ask
+* merge intervals and aggregate colors. output min intervals with unique color
 
-        return rets
+# Analysis
+* interval with value to aggregate -> sweepline
+1. convert intervals to events 
+2. to maintain order, we need to use ordered map or sort the collection
+3. aggregate events to merged intervals
+4. if required, merge intervals with same aggregated values
+"""
