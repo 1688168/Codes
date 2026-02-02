e755b82d2d0a5e04604b989db438a5a90aa36185
Y Lee <1688168@gmail.com>
2026-02-02 08:22:20 -0500

    1647

---
+++ LC/[1674] Minimum Moves to Make Array Complementary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int diff[2*100000+2];
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        int n = nums.size();
+        for(int ii=0; ii<n/2; ++ii){
+            int a = min(nums[ii], nums[n-1-ii]);
+            int b = max(nums[ii], nums[n-1-ii]);
+            diff[2] += 2;
+            diff[a+1] -= 1;
+            diff[a+b] -= 1;
+            diff[a+b+1] += 1;
+            diff[limit+b+1] += 1;
+        }
+
+        int y = 0;
+        int ret = INT_MAX;
+        for(int x=2; x<=limit*2; ++x){
+            y += diff[x];
+            ret = min(ret, y);
+        }
+        return ret;
+    }
+};
---
+++ LC/[1674] Minimum Moves to Make Array Complementary.md
@@ -0,0 +1,22 @@
+# 1674. Minimum Moves to Make Array Complementary
+
+## Problem Statements
+* nums: even length n
+* limit: replace with 1...limit (inclusive)
+* min moves required to make nums complementary
+* define operation
+
+## Analysis
+* N: 10^5 -> nlonn
+* what is the ultimate pair sum -> x: [2, 2*10^5] --- range of x 
+* if we know x, how many moves requires to make pairs sum to x
+* range arithmatic -> sweep line
+ex: 
+- 1, 2, 4, 3
+- limit=4
+
+
+## Ideas
+* greedy
+* dp
+* 
