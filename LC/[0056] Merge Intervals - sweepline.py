79c8713a1d66ffdf2ecababb5159f9cc2dcffc4a
Y Lee <1688168@gmail.com>
2026-05-11 00:25:48 -0400

    Update [0056] Merge Intervals - sweepline.py

--- LC/[0056] Merge Intervals - sweepline.py
+++ LC/[0056] Merge Intervals - sweepline.py
@@ -1,25 +1,30 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        events = []
+        """
+        * when dealing with intervals. (start, end) all represent events. -> sweepline
+        """
+        # build all events
+        events=[]
+        for ss, ee in intervals:
+            events.append((ss, 1))
+            events.append((ee, -1))
+        
+        # sort event start first
+        events.sort(key=lambda x: (x[0], -x[1])) # for same position, start with higher priority
 
-        for s, e in intervals:
-            events.append((s, 1))      # interval starts
-            events.append((e, -1))     # interval ends
+        # iterating over events, and output block when cnt=0 (no more overlapping blocks)
+        start=0
+        cnt=0
 
-        # Important: process starts before ends at the same point
-        events.sort(key=lambda x: (x[0], -x[1]))
+        res=[]
+        for xx, delta in events:
+            if cnt==0: #currently no block, starting a new block
+                start=xx
+            
+            cnt += delta # always accumulating counter
 
-        res = []
-        active = 0
-        start = None
-
-        for x, delta in events:
-            if active == 0:
-                start = x
-
-            active += delta
-
-            if active == 0:
-                res.append([start, x])
+            if cnt == 0: # when counter hit zero -> we finished a blcok
+                res.append([start, xx]) # output the block
 
         return res
+        
